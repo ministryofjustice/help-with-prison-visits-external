a23e4e4762b728158f0ea4204f9144ef7de4857c
Michael Hall <michaelha@square.local>
2016-10-11 13:58:48 +0100

    Deleted apply-upload, links to file-upload. Took upload out of Eligibility

--- app/routes/apply-upload.js
+++
@@ -1,5 +0,0 @@
-module.exports = function (router) {
-  router.get('/apply-upload', function (req, res) {
-    res.render('apply-upload')
-  })
-}
--- app/routes/file-upload.js
+++ app/routes/file-upload.js
@@ -1,5 +1,5 @@
 module.exports = function (router) {
   router.get('/file-upload', function (req, res) {
-    res.render('eligibility/file-upload')
+    res.render('file-upload')
   })
 }
