f4b41007c6b857808af665a553144bb969a0362c
Paul Wright <p.wright@kainos.com>
2016-10-11 17:01:12 +0100

    apvs-340 - added prettyprint and file logging

--- app/services/log.js
+++ app/services/log.js
@@ -1,12 +1,34 @@
 var bunyan = require('bunyan')
+var PrettyStream = require('bunyan-prettystream')
+var logsPath = process.env.LOGGING_PATH || 'logs/external-web.log'
+
+// Stream to handle pretty printing of Bunyan logs to stdout.
+var prettyStream = new PrettyStream()
+prettyStream.pipe(process.stdout)
 
 // Create a base logger for the application.
 var log = bunyan.createLogger({
   name: 'external-web',
+  streams: [],
   serializers: {
     'request': requestSerializer,
     'response': responseSerializer
   }
+})
+
+// Add console Stream.
+log.addStream({
+  level: 'DEBUG',
+  stream: prettyStream
+})
+
+// Add file stream.
+log.addStream({
+  type: 'rotating-file',
+  level: 'DEBUG',
+  path: logsPath,
+  period: '1d',
+  count: 7
 })
 
 function requestSerializer (request) {
