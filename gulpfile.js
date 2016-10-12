f70050f1deeccbb3e5cad4448589286d69b353a6
Paul Wright <p.wright@kainos.com>
2016-10-12 16:42:41 +0100

    chore - replaced build.sh with gulp tasks
    added show-hide-content.js
    moved assets out of and deleted app/public/
    updated readme

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp')
 var selenium = require('selenium-standalone')
 var webdriver = require('gulp-webdriver')
+var sass = require('gulp-sass')
 
 let seleniumServer
 
@@ -29,3 +30,51 @@ gulp.task('e2e', ['selenium'], () => {
       process.exit()
     })
 })
+
+gulp.task('assets', ['sass'], function () {
+  gulp.src('node_modules/govuk_frontend_toolkit/javascripts/**/*')
+    .pipe(gulp.dest('app/govuk_modules/govuk_frontend_toolkit/javascripts', { overwrite: true }))
+
+  gulp.src('node_modules/govuk_frontend_toolkit/images/**/*')
+    .pipe(gulp.dest('app/govuk_modules/govuk_frontend_toolkit/images', { overwrite: true }))
+
+  gulp.src('node_modules/govuk_template_jinja/assets/**/*')
+    .pipe(gulp.dest('app/govuk_modules/govuk_template/', { overwrite: true }))
+})
+
+gulp.task('templates', function () {
+  gulp.src('node_modules/govuk_template_jinja/views/layouts/govuk_template.html')
+    .pipe(gulp.dest('app/views/', { overwrite: true }))
+})
+
+gulp.task('sync', function () {
+  gulp.src('app/govuk_modules/govuk_frontend_toolkit/javascripts/govuk/show-hide-content.js')
+    .pipe(gulp.dest('app/public/javascripts/', { overwrite: true }))
+
+  gulp.src('app/assets/javascripts/**/*')
+    .pipe(gulp.dest('app/public/javascripts/', { overwrite: true }))
+})
+
+gulp.task('sass', function () {
+  gulp.src('app/assets/sass/**/*.scss')
+    .pipe(sass({
+      style: 'expanded',
+      sourcemap: true,
+      includePaths: [
+        'node_modules/govuk-elements-sass/public/sass',
+        'node_modules/govuk_frontend_toolkit/stylesheets',
+        'govuk_modules/govuk_template/assets/stylesheets',
+        'govuk_modules/govuk_frontend_toolkit/stylesheets',
+        'govuk_modules/govuk-elements-sass/'
+      ],
+      outputStyle: 'expanded'
+    }).on('error', sass.logError))
+    .pipe(gulp.dest('app/public/stylesheets'))
+})
+
+gulp.task('compile-and-copy-assets', [
+  'assets',
+  'templates',
+  'sync',
+  'sass'
+])
