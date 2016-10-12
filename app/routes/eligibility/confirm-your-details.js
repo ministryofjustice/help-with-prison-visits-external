ce140de20f3a5f2f53389df93381691c1844a7fd
Thomas Rowe <thomasrowe101@gmail.com>
2016-10-12 13:58:24 +0100

    Added POST route. Replaced a tag with a form including the question asked on the page. Routed user based on response to the question.

--- app/routes/eligibility/confirm-your-details.js
+++ app/routes/eligibility/confirm-your-details.js
@@ -2,4 +2,14 @@ module.exports = function (router) {
   router.get('/confirm-your-details', function (req, res) {
     res.render('eligibility/confirm-your-details')
   })
+
+  router.post('/confirm-your-details', function (req, res) {
+    var detailsChanged = req.body['details-changed']
+
+    if (detailsChanged === 'Yes') {
+      res.redirect('date-of-birth')
+    } else {
+      res.redirect('visit-type')
+    }
+  })
 }
