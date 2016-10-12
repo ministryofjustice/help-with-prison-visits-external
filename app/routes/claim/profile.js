038f06875eef8c501cdb1e5907cb4ef6b24fcf40
Thomas Rowe <thomasrowe101@gmail.com>
2016-10-12 13:48:44 +0100

    Added POST route.

--- app/routes/claim/profile.js
+++ app/routes/claim/profile.js
@@ -2,4 +2,8 @@ module.exports = function (router) {
   router.get('/profile', function (req, res) {
     res.render('claim/profile')
   })
+
+  router.post('/profile', function (req, res) {
+    res.redirect('confirm-your-details')
+  })
 }
