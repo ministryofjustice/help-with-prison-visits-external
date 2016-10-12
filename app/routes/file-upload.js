162181f264f9788635553253ef3062a8ca63e066
Thomas Rowe <thomasrowe101@gmail.com>
2016-10-12 13:33:56 +0100

    Added POST route.

--- app/routes/file-upload.js
+++ app/routes/file-upload.js
@@ -2,4 +2,8 @@ module.exports = function (router) {
   router.get('/file-upload', function (req, res) {
     res.render('file-upload')
   })
+
+  router.post('/file-upload', function (req, res) {
+    res.redirect('visit-type')
+  })
 }
