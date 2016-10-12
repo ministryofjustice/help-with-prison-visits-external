5ab5d55ef78f5a6d73b77f1a7980971c09bc0a59
Thomas Rowe <thomasrowe101@gmail.com>
2016-10-12 13:29:03 +0100

    Added POST route.

--- app/routes/eligibility/journey-assistance.js
+++ app/routes/eligibility/journey-assistance.js
@@ -2,4 +2,7 @@ module.exports = function (router) {
   router.get('/journey-assistance', function (req, res) {
     res.render('eligibility/journey-assistance')
   })
+  router.post('/journey-assistance', function (req, res) {
+    res.redirect('benefits')
+  })
 }
