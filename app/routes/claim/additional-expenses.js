e3aea51b7f43dbc1fc7c49967d122da7efe5366b
Thomas Rowe <thomasrowe101@gmail.com>
2016-10-12 13:42:22 +0100

    Added POST route.

--- app/routes/claim/additional-expenses.js
+++ app/routes/claim/additional-expenses.js
@@ -2,4 +2,8 @@ module.exports = function (router) {
   router.get('/additional-expenses', function (req, res) {
     res.render('claim/additional-expenses')
   })
+
+  router.post('/additional-expenses', function (req, res) {
+    res.redirect('light-refreshment-details')
+  })
 }
