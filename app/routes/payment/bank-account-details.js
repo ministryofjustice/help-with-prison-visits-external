bde2aa196e8935faac2ec4c22fcd2aed5c8cea07
Thomas Rowe <thomasrowe101@gmail.com>
2016-10-12 14:02:57 +0100

    Added POST route.

--- app/routes/payment/bank-account-details.js
+++ app/routes/payment/bank-account-details.js
@@ -2,4 +2,8 @@ module.exports = function (router) {
   router.get('/bank-account-details', function (req, res) {
     res.render('payment/bank-account-details')
   })
+
+  router.post('/bank-account-details', function (req, res) {
+    res.redirect('application-submitted')
+  })
 }
