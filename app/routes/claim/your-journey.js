dc0d887dd5b807a813f77809c4ebcbfd5a745b25
Thomas Rowe <thomasrowe101@gmail.com>
2016-10-12 13:36:50 +0100

    Added POST route.

--- app/routes/claim/your-journey.js
+++ app/routes/claim/your-journey.js
@@ -2,4 +2,8 @@ module.exports = function (router) {
   router.get('/your-journey', function (req, res) {
     res.render('claim/your-journey')
   })
+
+  router.post('/your-journey', function (req, res) {
+    res.redirect('transport')
+  })
 }
