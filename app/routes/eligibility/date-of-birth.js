0f51e7e9946b6c78bec0a1c2ac535d979c52ad36
Thomas Rowe <thomasrowe101@gmail.com>
2016-10-12 10:27:13 +0100

    The POST in prisoner-relationship now redirects as follows: - To journey assistance if the user selected escort. - To eligibility fail if they selected none of the above. - To benefits if they selected any other option. Added TODO for implementing the check on claimant age.

--- app/routes/eligibility/date-of-birth.js
+++ app/routes/eligibility/date-of-birth.js
@@ -2,4 +2,8 @@ module.exports = function (router) {
   router.get('/date-of-birth', function (req, res) {
     res.render('eligibility/date-of-birth')
   })
+
+
+  // TODO: Need age check here. If under 16 redirect to eligibility-fail.
+
 }
