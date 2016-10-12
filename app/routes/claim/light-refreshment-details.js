33b40cadf09b1385b72bfe33e62c4afbd724dcc5
Thomas Rowe <thomasrowe101@gmail.com>
2016-10-12 13:43:51 +0100

    Added POST route.

--- app/routes/claim/light-refreshment-details.js
+++ app/routes/claim/light-refreshment-details.js
@@ -2,4 +2,8 @@ module.exports = function (router) {
   router.get('/light-refreshment-details', function (req, res) {
     res.render('claim/light-refreshment-details')
   })
+
+  router.post('/light-refreshment-details', function (req, res) {
+    res.redirect('claim-summary')
+  })
 }
