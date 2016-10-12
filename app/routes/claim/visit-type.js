05cc6319b30ce96968638fa06fdc3ba2b83dc3ef
Thomas Rowe <thomasrowe101@gmail.com>
2016-10-12 13:35:03 +0100

    Added POST route.

--- app/routes/claim/visit-type.js
+++ app/routes/claim/visit-type.js
@@ -2,4 +2,8 @@ module.exports = function (router) {
   router.get('/visit-type', function (req, res) {
     res.render('claim/visit-type')
   })
+
+  router.post('/visit-type', function (req, res) {
+    res.redirect('your-journey')
+  })
 }
