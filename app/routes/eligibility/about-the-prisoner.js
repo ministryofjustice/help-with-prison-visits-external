a93e45c64350cd36015edfee9396cbaacc266ec6
Thomas Rowe <thomasrowe101@gmail.com>
2016-10-12 13:32:01 +0100

    Added POST route. Benefits on behalf now links to about the prisoner rather than file upload.

--- app/routes/eligibility/about-the-prisoner.js
+++ app/routes/eligibility/about-the-prisoner.js
@@ -2,4 +2,8 @@ module.exports = function (router) {
   router.get('/about-the-prisoner', function (req, res) {
     res.render('eligibility/about-the-prisoner')
   })
+
+  router.post('/about-the-prisoner', function (req, res) {
+    res.redirect('about-you')
+  })
 }
