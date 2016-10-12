751e6d822bc7c5787698aae3e72d3d47e48cb762
Thomas Rowe <thomasrowe101@gmail.com>
2016-10-12 13:38:56 +0100

    Added POST route.

--- app/routes/claim/car-details.js
+++ app/routes/claim/car-details.js
@@ -2,4 +2,8 @@ module.exports = function (router) {
   router.get('/car-details', function (req, res) {
     res.render('claim/car-details')
   })
+
+  router.post('/car-details', function (req, res) {
+    res.redirect('train-details')
+  })
 }
