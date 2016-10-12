73c720ec67898e3b382a69558c7658be1be85e27
Thomas Rowe <thomasrowe101@gmail.com>
2016-10-12 13:32:55 +0100

    Added POST route.

--- app/routes/eligibility/about-you.js
+++ app/routes/eligibility/about-you.js
@@ -2,4 +2,8 @@ module.exports = function (router) {
   router.get('/about-you', function (req, res) {
     res.render('eligibility/about-you')
   })
+
+  router.post('/about-you', function (req, res) {
+    res.redirect('file-upload')
+  })
 }
