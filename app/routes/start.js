177035cec2fec3e874500517d82d3e285bed85f3
Thomas Rowe <thomasrowe101@gmail.com>
2016-10-12 12:19:28 +0100

    Added POST routes for the start page.

--- app/routes/start.js
+++ app/routes/start.js
@@ -2,4 +2,12 @@ module.exports = function (router) {
   router.get('/start', function (req, res) {
     res.render('start')
   })
+
+  router.post('/first-time', function (req, res) {
+    res.redirect('date-of-birth')
+  })
+
+  router.post('/already-registered', function (req, res) {
+    res.redirect('profile')
+  })
 }
