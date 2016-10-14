ba7bb611b1488c9035d350fe62c4f7318ccc0f88
stevenalexander <steven.william.alexander@googlemail.com>
2016-10-14 17:04:36 +0100

    chore - moved e2e to separate gulpfile (#13)
    * chore - moved e2e to separate gulpfile

---
+++ gulpfile-e2e.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp')
+var selenium = require('selenium-standalone')
+var webdriver = require('gulp-webdriver')
+
+let seleniumServer
+
+gulp.task('selenium', (done) => {
+  selenium.install({logger: console.log}, () => {
+    selenium.start((err, child) => {
+      if (err) { return done(err) }
+      seleniumServer = child
+      done()
+    })
+  })
+})
+
+gulp.task('e2e', ['selenium'], () => {
+  return gulp.src('test/wdio.conf.js')
+    .pipe(webdriver()).on('error', () => {
+      seleniumServer.kill()
+      process.exit(1)
+    })
+    .once('error', function () { // Explicit exit for gulp-mocha
+      seleniumServer.kill()
+      process.exit(1)
+    })
+    .once('end', function () {
+      seleniumServer.kill()
+      process.exit()
+    })
+})
+
+gulp.task('default', ['e2e'])
