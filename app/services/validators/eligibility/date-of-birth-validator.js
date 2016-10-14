4b9f2b738ed6a6924122545ce5d9a5fd71eb34da
Thomas Rowe <thomasrowe101@gmail.com>
2016-10-14 16:27:12 +0100

    Added validation for the date-of-birth page Moved the validator test files and the files themselves into directories. To match the routes.

--- app/services/validators/eligibility/date-of-birth-validator.js
+++ app/services/validators/eligibility/date-of-birth-validator.js
@@ -1,4 +1,6 @@
-var FieldValidator = require('../field-validator')
+const FieldValidator = require('../field-validator')
+const FIELD_NAMES = require('../validation-field-names')
+const ERROR_MESSAGES = require('../validation-error-messages')
 
 class DateOfBirthValidator {
   static validate (data) {
@@ -12,29 +14,58 @@ class DateOfBirthValidator {
       .isRequired()
       .isNumeric()
       .isLength(2)
-      .isValidDay()
 
     FieldValidator(dobMonth, 'dob-month', errors)
       .isRequired()
       .isNumeric()
       .isLength(2)
-      .isValidMonth()
 
     FieldValidator(dobYear, 'dob-year', errors)
       .isRequired()
       .isNumeric()
       .isLength(4)
-      .isValidYear()
+
+    var dob = new Date(buildDOB(dobYear, dobMonth, dobDay))
+
+    if (!isValidDate(dob)) {
+      addErrorMessage(errors, 'dob', 'dob', ERROR_MESSAGES.getInvalidDobFormatMessage)
+    }
+
+    if (!isDateInThePast(dob)) {
+      addErrorMessage(errors, 'dob', 'dob', ERROR_MESSAGES.getFutureDobMessage)
+    }
 
     for (var field in errors) {
       if (errors.hasOwnProperty(field)) {
-        if (errors[field].length > 0) { return errors }
+        if (errors[ field ].length > 0) { return errors }
       }
     }
     return false
   }
 }
+
 exports.default = function (data) {
   return DateOfBirthValidator.validate(data)
 }
-module.exports = exports['default']
+module.exports = exports[ 'default' ]
+
+function buildDOB (year, month, day) {
+  return new Date(year + '-' + month + '-' + day)
+}
+
+function isValidDate (date) {
+  return date.toString() !== 'Invalid Date'
+}
+
+function isDateInThePast (date) {
+  return date <= new Date()
+}
+
+// TODO: This function should be moved to a generic class for adding errors.
+function addErrorMessage (errors, fieldName, displayName, message, options) {
+  if (!errors[fieldName]) {
+    errors[fieldName] = []
+  }
+  displayName = FIELD_NAMES[fieldName]
+  errors[fieldName].push(message(displayName, options))
+}
--- app/services/validators/validation-field-names.js
+++ app/services/validators/validation-field-names.js
@@ -1,6 +1,7 @@
 module.exports = {
   'relationship': 'Relationship',
-  'dob-day': 'Date of birth (day)',
-  'dob-month': 'Date of birth (month)',
-  'dob-year': 'Date of birth (year)'
+  'dob': 'Date of Birth',
+  'dob-day': 'Day',
+  'dob-month': 'Month',
+  'dob-year': 'Year'
 }
