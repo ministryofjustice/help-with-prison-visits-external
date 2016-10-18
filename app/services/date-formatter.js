25951f75498a2f078f9ea4f9fa22dc1f466c13f7
Thomas Rowe <thomasrowe101@gmail.com>
2016-10-18 11:23:27 +0100

    Added handling to the format function of date-formatter.js when passed a non Date object. Added handling to the buildFormatted function of date-formatter.js to handle the result of the call to this.build returning an invalid date. Added unit tests for date-formatter.js Mocha was only running tests in specific directories. Now runs recursively across all files, and sub directories of the test/unit directory.

--- app/services/date-formatter.js
+++ app/services/date-formatter.js
@@ -1,7 +1,11 @@
 const dateFormat = require('dateformat')
 const DATE_FORMAT = 'yyyy-mm-dd'
+const INVALID_DATE_ERROR = 'Invalid Date'
 
 exports.format = function (date) {
+  if (!isDate(date) && !isUndefined(date) && !isNull(date)) {
+    return INVALID_DATE_ERROR
+  }
   return dateFormat(date, DATE_FORMAT)
 }
 
@@ -10,5 +14,25 @@ exports.build = function (day, month, year) {
 }
 
 exports.buildFormatted = function (day, month, year) {
-  return this.format(this.build(day, month, year))
+  var date = this.build(day, month, year)
+  if (!isValidDate(date)) {
+    return INVALID_DATE_ERROR
+  }
+  return this.format(date)
+}
+
+function isUndefined (date) {
+  return typeof date === 'undefined'
+}
+
+function isNull (date) {
+  return date === null
+}
+
+function isDate (date) {
+  return date instanceof Date
+}
+
+function isValidDate (date) {
+  return date.toString() !== INVALID_DATE_ERROR
 }
