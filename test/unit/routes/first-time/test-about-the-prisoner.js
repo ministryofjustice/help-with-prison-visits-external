8e055d1ec689286e4915e75ab205dad385e6043b
Paul Wright <p.wright@kainos.com>
2016-10-19 11:01:11 +0100

    added validator to route and updated tests

--- test/unit/routes/first-time/test-about-the-prisoner.js
+++ test/unit/routes/first-time/test-about-the-prisoner.js
@@ -6,14 +6,17 @@ require('sinon-bluebird')
 var express = require('express')
 var mockViewEngine = require('../mock-view-engine')
 var firstTimeClaim = require('../../../../app/services/data/first-time-claim')
-
-var route = proxyquire('../../../../app/routes/first-time/about-the-prisoner', {
-  '../../services/data/first-time-claim': firstTimeClaim
-})
+var stubAboutThePrisonerValidator
 var request
 
 describe('routes/first-time/about-the-prisoner', function () {
   beforeEach(function () {
+    stubAboutThePrisonerValidator = sinon.stub()
+    var route = proxyquire('../../../../app/routes/first-time/about-the-prisoner', {
+      '../../services/data/first-time-claim': firstTimeClaim,
+      '../../services/validators/first-time/about-the-prisoner-validator': stubAboutThePrisonerValidator
+    })
+
     var app = express()
     mockViewEngine(app, '../../../app/views')
     route(app)
@@ -41,6 +44,7 @@ describe('routes/first-time/about-the-prisoner', function () {
     it('should persist data and redirect to first-time/about-you for valid data', function (done) {
       var newReference = '1234567'
       var stubInsertNewEligibilityAndPrisoner = sinon.stub(firstTimeClaim, 'insertNewEligibilityAndPrisoner').resolves(newReference)
+      stubAboutThePrisonerValidator.returns(false)
 
       request
         .post('/first-time/1980-01-01/partner/n/n')
@@ -48,7 +52,7 @@ describe('routes/first-time/about-the-prisoner', function () {
         .end(function (error, response) {
           expect(error).to.be.null
           // TODO check called path validator returning true
-          // TODO check called validator
+          expect(stubAboutThePrisonerValidator.calledOnce).to.be.true
           expect(stubInsertNewEligibilityAndPrisoner.calledOnce).to.be.true
           expect(response.header.location).to.equal(`/first-time/1980-01-01/partner/n/n/${newReference}`)
           done()
@@ -59,8 +63,15 @@ describe('routes/first-time/about-the-prisoner', function () {
       done()
     })
     it('should respond with a 400 for invalid data', function (done) {
-      // TODO stub validator and return false
-      done()
+      stubAboutThePrisonerValidator.returns({ 'firstName': [] })
+      request
+        .post('/first-time/1980-01-01/partner/n/n')
+        .expect(400)
+        .end(function (error, response) {
+          expect(error).to.be.null
+          expect(stubAboutThePrisonerValidator.calledOnce).to.be.true
+          done()
+        })
     })
   })
 })
