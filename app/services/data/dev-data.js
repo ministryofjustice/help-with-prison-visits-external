291dba118d1b175eb4dfab57b3cbb7712c6f9bc5
Steven Alexander <steven.william.alexander@googlemail.com>
2016-10-19 12:14:38 +0100

    chore - output prisoner/visitor

--- app/services/data/dev-data.js
+++ app/services/data/dev-data.js
@@ -3,9 +3,14 @@ var knex = require('knex')(config)
 
 module.exports.get = function (reference) {
   var data = {}
-  return knex.table('Eligibility').first().then(function (eligibility) {
+  return knex.table('Eligibility').where('Reference', reference).first().then(function (eligibility) {
     data.Eligibility = eligibility
-    console.log(eligibility)
-    return data
+    return knex.table('Prisoner').where('Reference', reference).first().then(function (prisoner) {
+      data.Prisoner = prisoner
+      return knex.table('Visitor').where('Reference', reference).first().then(function (visitor) {
+        data.Visitor = visitor
+        return data
+      })
+    })
   })
 }
