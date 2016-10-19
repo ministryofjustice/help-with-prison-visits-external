4bd73b935a3078511d5684841835a0a02406ec12
Paul Wright <wrightpaul08@gmail.com>
2016-10-19 14:20:00 +0100

    Feature/apvs 341/extend end to end test (#32)
    apvs-341 - extend end to end test to application submitted

--- test/e2e/spec.js
+++ test/e2e/spec.js
@@ -34,6 +34,35 @@ describe('First time claim flow', () => {
       .click('#income-support')
       .click('#benefit-submit')
 
+      // About the Prisoner
       .waitForExist('#about-the-prisoner-submit')
+      .setValue('#prisoner-first-name', 'Joe')
+      .setValue('#prisoner-last-name', 'Bloggs')
+      .setValue('#dob-day', '01')
+      .setValue('#dob-month', '05')
+      .setValue('#dob-year', '1955')
+      .setValue('#prisoner-number', 'A1234BC')
+      .setValue('#prison-name', 'Hewell')
+      .click('#about-the-prisoner-submit')
+
+      // About you
+      .waitForExist('#about-you-submit')
+      .setValue('#title-input', 'Mr')
+      .setValue('#first-name-input', 'Joe')
+      .setValue('#last-name-input', 'Bloggs')
+      .setValue('#national-insurance-number-input', 'AA123456A')
+      .setValue('#house-number-and-street-input', '1')
+      .setValue('#town-input', 'Town')
+      .setValue('#county-input', 'County')
+      .setValue('#post-code-input', 'AA123AA')
+      .click('#country-input')
+      .waitForExist('#country-ni')
+      .click('#country-ni')
+      .setValue('#email-address-input', 'test@test.com')
+      .setValue('#phone-number-input', '0123456789')
+      .click('#about-you-submit')
+
+      // Application Submitted
+      .waitForExist('#reference')
   })
 })
