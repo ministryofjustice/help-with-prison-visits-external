7d96be5d9931e6dbaa1f1d03249ab87aaf8a22c0
Steven Alexander <steven.william.alexander@googlemail.com>
2016-10-24 09:39:59 +0100

    apvs-346 - updated account num and sort code to use string as may start with leading zeros

--- migrations/20161024084923_add-claim-bank-details.js
+++ migrations/20161024084923_add-claim-bank-details.js
@@ -2,8 +2,8 @@ exports.up = function (knex, Promise) {
   return knex.schema.createTable('ClaimBankDetail', function (table) {
     table.increments('ClaimBankDetailId')
     table.integer('ClaimId').notNullable().references('Claim.ClaimId')
-    table.integer('AccountNumber')
-    table.integer('SortCode')
+    table.string('AccountNumber', 8)
+    table.string('SortCode', 6)
   })
 }
 
