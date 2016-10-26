2ae2c330a34f748caf5978dffd184e0c43371b25
Paul Wright <wrightpaul08@gmail.com>
2016-10-26 12:08:29 +0100

    Feature/apvs 346/add url validator for bank account details (#76)

--- app/routes/first-time/eligibility/claim/bank-account-details.js
+++ app/routes/first-time/eligibility/claim/bank-account-details.js
@@ -1,29 +1,30 @@
 const BankAccountDetails = require('../../../../services/domain/bank-account-details')
 const insertBankAccountDetailsForClaim = require('../../../../services/data/insert-bank-account-details-for-claim')
 const ValidationError = require('../../../../services/errors/validation-error')
+const UrlPathValidator = require('../../../../services/validators/url-path-validator')
 
 module.exports = function (router) {
   router.get('/first-time-claim/eligibility/:reference/claim/:claimId/bank-account-details', function (req, res) {
-    // TODO: Validate URL path
+    UrlPathValidator(req.params)
     return res.render('first-time/eligibility/claim/bank-account-details')
   })
 
   router.post('/first-time-claim/eligibility/:reference/claim/:claimId/bank-account-details', function (req, res) {
-    // TODO: Validate URL path
+    UrlPathValidator(req.params)
     try {
       var bankAccountDetails = new BankAccountDetails(req.body.AccountNumber, req.body.SortCode)
       insertBankAccountDetailsForClaim(req.params.claimId, bankAccountDetails)
         .then(function () {
           return res.redirect(`/application-submitted/${req.params.reference}`)
         })
-    } catch (e) {
-      if (e instanceof ValidationError) {
+    } catch (error) {
+      if (error instanceof ValidationError) {
         return res.status(400).render('first-time/eligibility/claim/bank-account-details', {
           bankDetails: req.body,
-          errors: e.validationErrors
+          errors: error.validationErrors
         })
       } else {
-        throw e
+        throw error
       }
     }
   })
--- test/unit/routes/first-time/eligibility/claim/test-bank-account-details.js
+++ test/unit/routes/first-time/eligibility/claim/test-bank-account-details.js
@@ -14,6 +14,7 @@ describe('routes/first-time/eligibility/claim/bank-account-details', function ()
   var request
   var stubBankAccountDetails
   var stubInsertBankAccountDetailsForClaim
+  var urlValidatorCalled
   const VALID_DATA = {
     'AccountNumber': '12345678',
     'SortCode': '123456'
@@ -26,7 +27,8 @@ describe('routes/first-time/eligibility/claim/bank-account-details', function ()
     var route = proxyquire(
       '../../../../../../app/routes/first-time/eligibility/claim/bank-account-details', {
         '../../../../services/domain/bank-account-details': stubBankAccountDetails,
-        '../../../../services/data/insert-bank-account-details-for-claim': stubInsertBankAccountDetailsForClaim
+        '../../../../services/data/insert-bank-account-details-for-claim': stubInsertBankAccountDetailsForClaim,
+        '../../../../services/validators/url-path-validator': function () { urlValidatorCalled = true }
       })
 
     var app = express()
@@ -34,6 +36,7 @@ describe('routes/first-time/eligibility/claim/bank-account-details', function ()
     mockViewEngine(app, '../../../app/views')
     route(app)
     request = supertest(app)
+    urlValidatorCalled = false
   })
 
   describe('GET /first-time-claim/eligibility/:reference/claim/:claimId/bank-account-details', function () {
@@ -41,7 +44,11 @@ describe('routes/first-time/eligibility/claim/bank-account-details', function ()
       request
         .get(`/first-time-claim/eligibility/${reference}/claim/${claimId}/bank-account-details`)
         .expect(200)
-        .end(done)
+        .end(function (error, response) {
+          expect(error).to.be.null
+          expect(urlValidatorCalled).to.be.true
+          done()
+        })
     })
   })
 
@@ -57,6 +64,7 @@ describe('routes/first-time/eligibility/claim/bank-account-details', function ()
         .expect(302)
         .end(function (error, response) {
           expect(error).to.be.null
+          expect(urlValidatorCalled).to.be.true
           expect(stubBankAccountDetails.calledWithExactly(VALID_DATA.AccountNumber, VALID_DATA.SortCode)).to.be.true
           expect(stubInsertBankAccountDetailsForClaim.calledWithExactly(claimId, newBankAccountDetails)).to.be.true
           expect(response.headers['location']).to.be.equal('/application-submitted/' + reference)
@@ -69,7 +77,11 @@ describe('routes/first-time/eligibility/claim/bank-account-details', function ()
       request
         .post(`/first-time-claim/eligibility/${reference}/claim/${claimId}/bank-account-details`)
         .expect(400)
-        .end(done)
+        .end(function (error, response) {
+          expect(error).to.be.null
+          expect(urlValidatorCalled).to.be.true
+          done()
+        })
     })
   })
 })
