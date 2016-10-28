728cc82057876da50f56a04fa1e470f6e5b0fa75
Thomas Rowe <thomasrowe101@gmail.com>
2016-10-28 17:23:20 +0100

    Added validation to the plane expense page.

--- app/routes/first-time/eligibility/claim/plane-details.js
+++ app/routes/first-time/eligibility/claim/plane-details.js
@@ -1,4 +1,5 @@
 const UrlPathValidator = require('../../../../services/validators/url-path-validator')
+const ValidationError = require('../../../../services/errors/validation-error')
 const expenseUrlRouter = require('../../../../services/routing/expenses-url-router')
 const PlaneExpense = require('../../../../services/domain/expenses/plane-expense')
 const insertExpense = require('../../../../services/data/insert-expense')
@@ -16,17 +17,30 @@ module.exports = function (router) {
   router.post('/first-time-claim/eligibility/:reference/claim/:claimId/plane', function (req, res) {
     UrlPathValidator(req.params)
 
-    var expense = new PlaneExpense(
-      req.params.claimId,
-      req.body.cost,
-      req.body.from,
-      req.body.to,
-      req.body['return-journey']
-    )
+    try {
+      var expense = new PlaneExpense(
+        req.params.claimId,
+        req.body.cost,
+        req.body.from,
+        req.body.to,
+        req.body['return-journey']
+      )
 
-    insertExpense(expense)
-      .then(function () {
-        return res.redirect(expenseUrlRouter.getRedirectUrl(req))
-      })
+      insertExpense(expense)
+        .then(function () {
+          return res.redirect(expenseUrlRouter.getRedirectUrl(req))
+        })
+    } catch (error) {
+      if (error instanceof ValidationError) {
+        return res.status(400).render('first-time/eligibility/claim/plane-details', {
+          errors: error.validationErrors,
+          reference: req.params.reference,
+          claimId: req.params.claimId,
+          expense: req.body
+        })
+      } else {
+        throw error
+      }
+    }
   })
 }
