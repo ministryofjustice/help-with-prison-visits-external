46ef202a72afeb49ea40320773e4729131478bc4
Thomas Rowe <thomasrowe101@gmail.com>
2016-10-31 11:29:44 +0000

    Added validation to the ferry expense page.

--- app/routes/first-time/eligibility/claim/ferry-details.js
+++ app/routes/first-time/eligibility/claim/ferry-details.js
@@ -1,4 +1,5 @@
 const UrlPathValidator = require('../../../../services/validators/url-path-validator')
+const ValidationError = require('../../../../services/errors/validation-error')
 const expenseUrlRouter = require('../../../../services/routing/expenses-url-router')
 const FerryExpense = require('../../../../services/domain/expenses/ferry-expense')
 const insertExpense = require('../../../../services/data/insert-expense')
@@ -16,18 +17,31 @@ module.exports = function (router) {
   router.post('/first-time-claim/eligibility/:reference/claim/:claimId/ferry', function (req, res) {
     UrlPathValidator(req.params)
 
-    var expense = new FerryExpense(
-      req.params.claimId,
-      req.body.cost,
-      req.body.from,
-      req.body.to,
-      req.body['return-journey'],
-      req.body['ticket-type']
-    )
+    try {
+      var expense = new FerryExpense(
+        req.params.claimId,
+        req.body.cost,
+        req.body.from,
+        req.body.to,
+        req.body['return-journey'],
+        req.body['ticket-type']
+      )
 
-    insertExpense(expense)
-      .then(function () {
-        return res.redirect(expenseUrlRouter.getRedirectUrl(req))
-      })
+      insertExpense(expense)
+        .then(function () {
+          return res.redirect(expenseUrlRouter.getRedirectUrl(req))
+        })
+    } catch (error) {
+      if (error instanceof ValidationError) {
+        return res.status(400).render('first-time/eligibility/claim/ferry-details', {
+          errors: error.validationErrors,
+          reference: req.params.reference,
+          claimId: req.params.claimId,
+          expense: req.body
+        })
+      } else {
+        throw error
+      }
+    }
   })
 }
--- app/services/domain/expenses/ferry-expense.js
+++ app/services/domain/expenses/ferry-expense.js
@@ -1,5 +1,8 @@
 const BaseExpense = require('../../../services/domain/expenses/base-expense')
 const EXPENSE_TYPE = require('../../../constants/expense-type-enum')
+const ValidationError = require('../../errors/validation-error')
+const FieldValidator = require('../../validators/field-validator')
+const ErrorHandler = require('../../validators/error-handler')
 
 class FerryExpense extends BaseExpense {
   constructor (claimId, cost, from, to, returnJourney, ticketType) {
@@ -8,7 +11,31 @@ class FerryExpense extends BaseExpense {
   }
 
   isValid () {
-    // TODO: Implement validation.
+    var errors = ErrorHandler()
+
+    FieldValidator(this.from, 'from', errors)
+      .isRequired()
+      .isLessThanLength(100)
+
+    FieldValidator(this.to, 'to', errors)
+      .isRequired()
+      .isLessThanLength(100)
+
+    FieldValidator(this.isReturn, 'return-journey', errors)
+      .isRequired()
+
+    FieldValidator(this.ticketType, 'ticket-type', errors)
+      .isRequired()
+
+    FieldValidator(this.cost, 'cost', errors)
+      .isRequired()
+      .isCurrency()
+      .isGreaterThanZero()
+
+    var validationErrors = errors.get()
+    if (validationErrors) {
+      throw new ValidationError(validationErrors)
+    }
   }
 }
 
