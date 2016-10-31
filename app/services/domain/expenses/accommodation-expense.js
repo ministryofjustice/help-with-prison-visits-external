d8671ef62dea768550202f4ff903122401f769da
Thomas Rowe <thomasrowe101@gmail.com>
2016-10-31 11:43:17 +0000

    Added validation to the accommodation expense page.

--- app/services/domain/expenses/accommodation-expense.js
+++ app/services/domain/expenses/accommodation-expense.js
@@ -1,5 +1,8 @@
 const BaseExpense = require('../../../services/domain/expenses/base-expense')
 const EXPENSE_TYPE = require('../../../constants/expense-type-enum')
+const ValidationError = require('../../errors/validation-error')
+const FieldValidator = require('../../validators/field-validator')
+const ErrorHandler = require('../../validators/error-handler')
 
 class AccommodationExpense extends BaseExpense {
   constructor (claimId, cost, durationOfTravel) {
@@ -8,7 +11,22 @@ class AccommodationExpense extends BaseExpense {
   }
 
   isValid () {
-    // TODO: Implement validation.
+    var errors = ErrorHandler()
+
+    FieldValidator(this.durationOfTravel, 'duration', errors)
+      .isRequired()
+      .isNumeric()
+      .isGreaterThanZero()
+
+    FieldValidator(this.cost, 'cost', errors)
+      .isRequired()
+      .isCurrency()
+      .isGreaterThanZero()
+
+    var validationErrors = errors.get()
+    if (validationErrors) {
+      throw new ValidationError(validationErrors)
+    }
   }
 }
 
