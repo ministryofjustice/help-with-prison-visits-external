7a49f0f4431cb7bfa3b127efccb10139086b8897
Steven Alexander <steven.william.alexander@googlemail.com>
2016-10-31 14:52:01 +0000

    apvs-351 - added claim details to summary

--- app/routes/first-time/eligibility/claim/claim-summary.js
+++ app/routes/first-time/eligibility/claim/claim-summary.js
@@ -1,14 +1,19 @@
 const UrlPathValidator = require('../../../../services/validators/url-path-validator')
+const getIndividualClaimDetails = require('../../../../services/data/get-individual-claim-details')
 
 module.exports = function (router) {
   router.get('/first-time-claim/eligibility/:reference/claim/:claimId/summary', function (req, res) {
     UrlPathValidator(req.params)
-    return res.render('first-time/eligibility/claim/claim-summary',
-      {
-        reference: req.params.reference,
-        claimId: req.params.claimId
-      }
-    )
+
+    getIndividualClaimDetails(req.params.claimId)
+      .then(function (claimDetails) {
+        return res.render('first-time/eligibility/claim/claim-summary',
+          {
+            reference: req.params.reference,
+            claimId: req.params.claimId,
+            claimDetails: claimDetails
+          })
+      })
   })
 
   router.post('/first-time-claim/eligibility/:reference/claim/:claimId/summary', function (req, res) {
---
+++ app/services/data/get-individual-claim-details.js
@@ -0,0 +1,26 @@
+const config = require('../../../knexfile').extweb
+const knex = require('knex')(config)
+
+// TODO test
+module.exports = function (claimId) {
+  return knex('Claim')
+    .join('Eligibility', 'Claim.Reference', '=', 'Eligibility.Reference')
+    .join('Visitor', 'Eligibility.Reference', '=', 'Visitor.Reference')
+    .join('Prisoner', 'Eligibility.Reference', '=', 'Prisoner.Reference')
+    .where('Claim.ClaimId', claimId)
+    .first('Eligibility.Reference', 'Claim.DateSubmitted', 'Claim.DateOfJourney', 'Visitor.FirstName', 'Visitor.LastName',
+      'Visitor.DateOfBirth', 'Visitor.NationalInsuranceNumber', 'Visitor.HouseNumberAndStreet', 'Visitor.Town', 'Visitor.County', 'Visitor.PostCode',
+      'Visitor.EmailAddress', 'Visitor.PhoneNumber', 'Visitor.Relationship', 'Prisoner.FirstName AS PrisonerFirstName', 'Prisoner.LastName AS PrisonerLastName',
+      'Prisoner.DateOfBirth AS PrisonerDateOfBirth', 'Prisoner.PrisonNumber', 'Prisoner.NameOfPrison')
+    .then(function (claim) {
+      return knex('Claim')
+        .join('ClaimExpense', 'Claim.ClaimId', '=', 'ClaimExpense.ClaimId')
+        .where('Claim.ClaimId', claimId)
+        .select('ClaimExpense.ClaimExpenseId', 'ClaimExpense.ExpenseType', 'ClaimExpense.Cost', 'ClaimExpense.To', 'ClaimExpense.From', 'ClaimExpense.IsReturn', 'ClaimExpense.TravelTime',
+          'ClaimExpense.DurationOfTravel', 'ClaimExpense.TicketType')
+        .orderBy('ClaimExpense.ClaimExpenseId')
+        .then(function (claimExpenses) {
+          return {claim: claim, claimExpenses: claimExpenses}
+        })
+    })
+}
