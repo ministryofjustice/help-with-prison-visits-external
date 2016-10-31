42a507a65a494b051634ad724b4ccf0b772ffb96
Thomas Rowe <thomasrowe101@gmail.com>
2016-10-31 11:36:49 +0000

    Added validation to the light refreshment expense page.

--- app/routes/first-time/eligibility/claim/light-refreshment-details.js
+++ app/routes/first-time/eligibility/claim/light-refreshment-details.js
@@ -1,4 +1,5 @@
 const UrlPathValidator = require('../../../../services/validators/url-path-validator')
+const ValidationError = require('../../../../services/errors/validation-error')
 const expenseUrlRouter = require('../../../../services/routing/expenses-url-router')
 const RefreshmentExpense = require('../../../../services/domain/expenses/refreshment-expense')
 const insertExpense = require('../../../../services/data/insert-expense')
@@ -16,15 +17,28 @@ module.exports = function (router) {
   router.post('/first-time-claim/eligibility/:reference/claim/:claimId/refreshment', function (req, res) {
     UrlPathValidator(req.params)
 
-    var expense = new RefreshmentExpense(
-      req.params.claimId,
-      req.body.cost,
-      req.body['travel-time']
-    )
+    try {
+      var expense = new RefreshmentExpense(
+        req.params.claimId,
+        req.body.cost,
+        req.body['travel-time']
+      )
 
-    insertExpense(expense)
-      .then(function () {
-        return res.redirect(expenseUrlRouter.getRedirectUrl(req))
-      })
+      insertExpense(expense)
+        .then(function () {
+          return res.redirect(expenseUrlRouter.getRedirectUrl(req))
+        })
+    } catch (error) {
+      if (error instanceof ValidationError) {
+        return res.status(400).render('first-time/eligibility/claim/light-refreshment-details', {
+          errors: error.validationErrors,
+          reference: req.params.reference,
+          claimId: req.params.claimId,
+          expense: req.body
+        })
+      } else {
+        throw error
+      }
+    }
   })
 }
--- app/services/domain/expenses/refreshment-expense.js
+++ app/services/domain/expenses/refreshment-expense.js
@@ -1,5 +1,8 @@
 const BaseExpense = require('../../../services/domain/expenses/base-expense')
 const EXPENSE_TYPE = require('../../../constants/expense-type-enum')
+const ValidationError = require('../../errors/validation-error')
+const FieldValidator = require('../../validators/field-validator')
+const ErrorHandler = require('../../validators/error-handler')
 
 class RefreshmentExpense extends BaseExpense {
   constructor (claimId, cost, travelTime) {
@@ -8,7 +11,20 @@ class RefreshmentExpense extends BaseExpense {
   }
 
   isValid () {
-    // TODO: Implement validation.
+    var errors = ErrorHandler()
+
+    FieldValidator(this.travelTime, 'travel-time', errors)
+      .isRequired()
+
+    FieldValidator(this.cost, 'cost', errors)
+      .isRequired()
+      .isCurrency()
+      .isGreaterThanZero()
+
+    var validationErrors = errors.get()
+    if (validationErrors) {
+      throw new ValidationError(validationErrors)
+    }
   }
 }
 
