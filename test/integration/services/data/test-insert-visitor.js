113f8dd8640fb895f57bef0843509a78c0cdad00
Thomas Rowe <thomasrowe101@gmail.com>
2016-11-01 14:34:46 +0000

    Test now uses helpers.

--- test/integration/services/data/test-insert-visitor.js
+++ test/integration/services/data/test-insert-visitor.js
@@ -1,60 +1,39 @@
-var expect = require('chai').expect
-var config = require('../../../../knexfile').migrations
-var knex = require('knex')(config)
-var moment = require('moment')
-
-var insertVisitor = require('../../../../app/services/data/insert-visitor')
-var reference = 'V123456'
+const expect = require('chai').expect
+const moment = require('moment')
+const eligiblityHelper = require('../../../helpers/data/eligibility-helper')
+const visitorHelper = require('../../../helpers/data/visitor-helper')
 
 describe('services/data/insert-visitor', function () {
-  describe('insert', function () {
-    before(function () {
-      return knex('ExtSchema.Eligibility').insert({
-        Reference: reference,
-        DateCreated: moment().toDate(),
-        Status: 'TEST'
+  var REFERENCE = 'V123456'
+
+  before(function () {
+    return eligiblityHelper.insert(REFERENCE)
+  })
+
+  it('should insert a new Visitor for a reference', function () {
+    return visitorHelper.insert(REFERENCE)
+      .then(function () {
+        return visitorHelper.get(REFERENCE)
       })
-    })
-
-    it('should insert a new Visitor for a reference', function () {
-      var visitorData = {
-        Title: 'Mr',
-        FirstName: 'John  ',
-        LastName: '  Smith',
-        NationalInsuranceNumber: 'QQ 12 34 56 c',
-        HouseNumberAndStreet: '1 Test Road',
-        Town: '1 Test Road',
-        County: 'Durham',
-        PostCode: 'bT11 1BT',
-        Country: 'England',
-        EmailAddress: 'test@test.com',
-        PhoneNumber: '07911111111',
-        DateOfBirth: '1980-02-01',
-        Relationship: 'partner',
-        JourneyAssistance: 'no',
-        Benefit: 'income-support'
-      }
-
-      return insertVisitor(reference, visitorData)
-        .then(function () {
-          knex.select().from('ExtSchema.Visitor').where('Reference', reference).then(function (results) {
-            expect(results.length).to.equal(1)
-            expect(results[0].Reference).to.equal(reference)
-            expect(results[0].FirstName).to.equal('John')
-            expect(results[0].LastName).to.equal('Smith')
-            expect(results[0].NationalInsuranceNumber).to.equal('QQ123456C')
-            expect(results[0].DateOfBirth.toDateString()).to.equal('Fri Feb 01 1980')
-            expect(results[0].PostCode).to.equal('BT111BT')
-            expect(results[0].Benefit).to.equal('income-support')
-            expect(results[0].RequireBenefitUpload, 'should set RequireBenefitUpload based on benefit').to.be.false
-          })
-        })
-    })
-
-    after(function () {
-      return knex('ExtSchema.Visitor').where('Reference', reference).del().then(function () {
-        return knex('ExtSchema.Eligibility').where('Reference', reference).del()
+      .then(function (visitor) {
+        expect(visitor.Reference).to.equal(REFERENCE)
+        expect(visitor.FirstName).to.equal(visitorHelper.FIRST_NAME)
+        expect(visitor.LastName).to.equal(visitorHelper.LAST_NAME)
+        expect(visitor.NationalInsuranceNumber).to.equal(visitorHelper.NATIONAL_INSURANCE_NUMBER)
+        expect(visitor.DateOfBirth).to.be.within(
+          moment(visitorHelper.DATE_OF_BIRTH).subtract(1, 'seconds').toDate(),
+          moment(visitorHelper.DATE_OF_BIRTH).add(1, 'seconds').toDate()
+        )
+        expect(visitor.PostCode).to.equal(visitorHelper.POST_CODE)
+        expect(visitor.Benefit).to.equal(visitorHelper.BENEFIT)
+        expect(visitor.RequireBenefitUpload, 'should set RequireBenefitUpload based on benefit').to.be.false
       })
-    })
+  })
+
+  after(function () {
+    return visitorHelper.delete(REFERENCE)
+      .then(function () {
+        return eligiblityHelper.delete(REFERENCE)
+      })
   })
 })
