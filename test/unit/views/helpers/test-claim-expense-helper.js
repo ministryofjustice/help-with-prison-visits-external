8b95f66733e13d93ad193124058ee4412b4288b8
Steven Alexander <steven.william.alexander@googlemail.com>
2016-11-02 08:49:51 +0000

    chore - added test for claim-expense-helper

--- test/unit/views/helpers/test-claim-expense-helper.js
+++ test/unit/views/helpers/test-claim-expense-helper.js
@@ -13,13 +13,33 @@ describe('views/helpers/claim-expense-helper', function () {
 
   describe('FormattedDetail', function () {
     it('should return formatted detail string for ClaimExpense', function (done) {
-      var claimExpense = {
-        ExpenseType: 'car',
-        From: 'PointA',
-        To: 'PointB'
-      }
+      const from = 'PointA'
+      const to = 'PointB'
+      const durationOfTravel = 2
 
-      expect(claimExpenseHelper.FormattedDetail(claimExpense)).to.equal('PointA to PointB')
+      expect(claimExpenseHelper.FormattedDetail({ExpenseType: 'car hire', From: from, To: to, DurationOfTravel: 2})).to.equal(
+        `${from} to ${to} for ${durationOfTravel} days`)
+
+      expect(claimExpenseHelper.FormattedDetail({ExpenseType: 'bus', From: from, To: to})).to.equal(
+        `${from} to ${to}`)
+
+      expect(claimExpenseHelper.FormattedDetail({ExpenseType: 'train', From: from, To: to, IsReturn: true})).to.equal(
+        `${from} to ${to} - Return`)
+
+      expect(claimExpenseHelper.FormattedDetail({ExpenseType: 'light refreshment', TravelTime: 'over-five'})).to.equal(
+        'Over five hours away but under ten hours')
+
+      expect(claimExpenseHelper.FormattedDetail({ExpenseType: 'light refreshment', TravelTime: 'over-ten'})).to.equal(
+        'Over ten hours away')
+
+      expect(claimExpenseHelper.FormattedDetail({ExpenseType: 'accommodation', DurationOfTravel: durationOfTravel})).to.equal(
+        `Nights stayed: ${durationOfTravel}`)
+
+      expect(claimExpenseHelper.FormattedDetail({ExpenseType: 'ferry', From: from, To: to, TicketType: 'foot-passenger'})).to.equal(
+        `${from} to ${to} as a foot passenger`)
+
+      expect(claimExpenseHelper.FormattedDetail({ExpenseType: 'anything-else', From: from, To: to})).to.equal(
+        `${from} to ${to}`)
 
       done()
     })
