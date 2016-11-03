3aff30b97b8cc0796c6c2f264e93421f28a4884f
stevenalexander <steven.william.alexander@googlemail.com>
2016-11-03 10:01:42 +0000

    chore - added test for claim-expense-helper (#110)
    * chore - added test for claim-expense-helper

--- test/unit/views/helpers/test-claim-expense-helper.js
+++ test/unit/views/helpers/test-claim-expense-helper.js
@@ -4,24 +4,40 @@ const claimExpenseHelper = require('../../../../app/views/helpers/claim-expense-
 
 describe('views/helpers/claim-expense-helper', function () {
   describe('DisplayName', function () {
-    it('should return display name for ClaimExpense', function (done) {
+    it('should return display name for ClaimExpense', function () {
       expect(claimExpenseHelper.DisplayName({ExpenseType: 'car'})).to.equal('Car Journey')
-
-      done()
     })
   })
 
   describe('FormattedDetail', function () {
-    it('should return formatted detail string for ClaimExpense', function (done) {
-      var claimExpense = {
-        ExpenseType: 'car',
-        From: 'PointA',
-        To: 'PointB'
-      }
+    it('should return formatted detail string for ClaimExpense', function () {
+      const from = 'PointA'
+      const to = 'PointB'
+      const durationOfTravel = 2
 
-      expect(claimExpenseHelper.FormattedDetail(claimExpense)).to.equal('PointA to PointB')
+      expect(claimExpenseHelper.FormattedDetail({ExpenseType: 'car hire', From: from, To: to, DurationOfTravel: 2})).to.equal(
+        `${from} to ${to} for ${durationOfTravel} days`)
 
-      done()
+      expect(claimExpenseHelper.FormattedDetail({ExpenseType: 'bus', From: from, To: to})).to.equal(
+        `${from} to ${to}`)
+
+      expect(claimExpenseHelper.FormattedDetail({ExpenseType: 'train', From: from, To: to, IsReturn: true})).to.equal(
+        `${from} to ${to} - Return`)
+
+      expect(claimExpenseHelper.FormattedDetail({ExpenseType: 'light refreshment', TravelTime: 'over-five'})).to.equal(
+        'Over five hours away but under ten hours')
+
+      expect(claimExpenseHelper.FormattedDetail({ExpenseType: 'light refreshment', TravelTime: 'over-ten'})).to.equal(
+        'Over ten hours away')
+
+      expect(claimExpenseHelper.FormattedDetail({ExpenseType: 'accommodation', DurationOfTravel: durationOfTravel})).to.equal(
+        `Nights stayed: ${durationOfTravel}`)
+
+      expect(claimExpenseHelper.FormattedDetail({ExpenseType: 'ferry', From: from, To: to, TicketType: 'foot-passenger'})).to.equal(
+        `${from} to ${to} as a foot passenger`)
+
+      expect(claimExpenseHelper.FormattedDetail({ExpenseType: 'anything-else', From: from, To: to})).to.equal(
+        `${from} to ${to}`)
     })
   })
 })
