02601c671ed50c3eceafe155a6cf6736dfca0a94
stevenalexander <steven.william.alexander@googlemail.com>
2016-11-04 14:07:26 +0000

    Chore add test get individual claim details (#126)
    * refactored insert-first-time-claim to return claimId and removed test helper CLAIM_ID which would not work in parallel
    
    * renamed data method to get-claim-summary
    
    * chore - added missing test for get-claim-summary

--- test/helpers/data/claim-helper.js
+++ test/helpers/data/claim-helper.js
@@ -4,14 +4,12 @@ const moment = require('moment')
 const insertFirstTimeClaim = require('../../../app/services/data/insert-first-time-claim')
 const FirstTimeClaim = require('../../../app/services/domain/first-time-claim')
 const claimStatusEnum = require('../../../app/constants/claim-status-enum')
-const dateFormatter = require('../../../app/services/date-formatter')
 
-module.exports.DATE_OF_JOURNEY = moment().toDate()
-module.exports.DATE_OF_JOURNEY_FORMATTED = dateFormatter.build(
-  this.DATE_OF_JOURNEY.getDay(),
-  this.DATE_OF_JOURNEY.getMonth(),
-  this.DATE_OF_JOURNEY.getFullYear()
-)
+const DAY = '01'
+const MONTH = '11'
+const YEAR = '2016'
+
+module.exports.DATE_OF_JOURNEY = moment(`${YEAR}-${MONTH}-${DAY}`).toDate()
 module.exports.DATE_CREATED = moment().toDate()
 module.exports.DATE_SUBMITTED = moment().toDate()
 module.exports.STATUS = claimStatusEnum.IN_PROGRESS
@@ -19,17 +17,14 @@ module.exports.STATUS = claimStatusEnum.IN_PROGRESS
 module.exports.build = function (reference) {
   return new FirstTimeClaim(
     reference,
-    this.DATE_OF_JOURNEY.getDay(),
-    this.DATE_OF_JOURNEY.getMonth(),
-    this.DATE_OF_JOURNEY.getFullYear()
+    DAY,
+    MONTH,
+    YEAR
   )
 }
 
 module.exports.insert = function (reference) {
   return insertFirstTimeClaim(this.build(reference))
-    .then(function (claimId) {
-      module.exports.CLAIM_ID = claimId[0]
-    })
 }
 
 module.exports.get = function (claimId) {
