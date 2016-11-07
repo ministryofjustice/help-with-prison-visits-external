ef1a3296293de49969a6dfaa520c19a66d79da71
Thomas Rowe <thomasrowe101@gmail.com>
2016-11-07 11:00:44 +0000

    Updated route, unit test, and HTML.

--- test/unit/routes/first-time/eligibility/claim/test-ferry-details.js
+++ test/unit/routes/first-time/eligibility/claim/test-ferry-details.js
@@ -7,7 +7,7 @@ require('sinon-bluebird')
 const ValidationError = require('../../../../../../app/services/errors/validation-error')
 
 describe('routes/first-time/eligibility/claim/ferry-details', function () {
-  const ROUTE = `/first-time-claim/eligibility/A123456/claim/1/ferry`
+  const ROUTE = `/first-time/eligibility/A123456/claim/1/ferry`
 
   var app
 
