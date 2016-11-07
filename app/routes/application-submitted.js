3b050cf6b28cc4381a4e06896f31c3d4dbc9d6c2
Michael Hall <m.hall@kainos.com>
2016-11-07 12:56:55 +0000

    Chore/adding test to application submitted (#129)
    Added URL validator to date submitted
    Added route unit tests for application submitted

--- app/routes/application-submitted.js
+++ app/routes/application-submitted.js
@@ -1,5 +1,8 @@
+const UrlPathValidator = require('../services/validators/url-path-validator')
+
 module.exports = function (router) {
   router.get('/application-submitted/:reference', function (req, res) {
+    UrlPathValidator(req.params)
     return res.render('application-submitted', {
       reference: req.params.reference
     })
---
+++ test/unit/routes/test-application-submitted.js
@@ -0,0 +1,36 @@
+const routeHelper = require('../../helpers/routes/route-helper')
+const supertest = require('supertest')
+const proxyquire = require('proxyquire')
+const sinon = require('sinon')
+require('sinon-bluebird')
+
+describe('routes/application-submitted', function () {
+  const ROUTE = `/application-submitted/RA2E0QZ`
+  var app
+  var urlPathValidatorStub
+
+  beforeEach(function () {
+    urlPathValidatorStub = sinon.stub()
+
+    var route = proxyquire('../../../app/routes/application-submitted', {
+      '../services/validators/url-path-validator': urlPathValidatorStub
+    })
+    app = routeHelper.buildApp(route)
+  })
+
+  describe(`GET ${ROUTE}`, function () {
+    it('should call the URL Path Validator', function () {
+      return supertest(app)
+        .get(ROUTE)
+        .expect(function () {
+          sinon.assert.calledOnce(urlPathValidatorStub)
+        })
+    })
+
+    it('should respond with a 200', function () {
+      return supertest(app)
+        .get(ROUTE)
+        .expect(200)
+    })
+  })
+})
