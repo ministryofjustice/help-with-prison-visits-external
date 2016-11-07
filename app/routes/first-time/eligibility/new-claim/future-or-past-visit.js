ce32ce10595b030f2629d0b0160cab32493f1a07
Thomas Rowe <thomasrowe101@gmail.com>
2016-11-07 09:43:56 +0000

    Updated route, unit tests, and HTML.

--- app/routes/first-time/eligibility/new-claim/future-or-past-visit.js
+++ app/routes/first-time/eligibility/new-claim/future-or-past-visit.js
@@ -1,15 +1,15 @@
 const UrlPathValidator = require('../../../../services/validators/url-path-validator')
 
 module.exports = function (router) {
-  router.get('/first-time-claim/eligibility/:reference/new-claim', function (req, res) {
+  router.get('/first-time/eligibility/:reference/new-claim', function (req, res) {
     UrlPathValidator(req.params)
     return res.render('first-time/eligibility/new-claim/future-or-past-visit', {
       reference: req.params.reference
     })
   })
 
-  router.post('/first-time-claim/eligibility/:reference/new-claim', function (req, res) {
+  router.post('/first-time/eligibility/:reference/new-claim', function (req, res) {
     UrlPathValidator(req.params)
-    return res.redirect(`/first-time-claim/eligibility/${req.params.reference}/new-claim/past`)
+    return res.redirect(`/first-time/eligibility/${req.params.reference}/new-claim/past`)
   })
 }
--- test/unit/routes/first-time/eligibility/new-claim/test-future-or-past-visit.js
+++ test/unit/routes/first-time/eligibility/new-claim/test-future-or-past-visit.js
@@ -7,6 +7,8 @@ const mockViewEngine = require('../../../mock-view-engine')
 var request
 
 describe('routes/first-time/eligibility/new-claim/future-or-past-visit', function () {
+  const REFERENCE = '1234567'
+  const ROUTE = `/first-time/eligibility/${REFERENCE}/new-claim`
   var urlValidatorCalled = false
 
   beforeEach(function () {
@@ -24,7 +26,7 @@ describe('routes/first-time/eligibility/new-claim/future-or-past-visit', functio
   describe('GET /first-time/eligibility/:reference/new-claim', function () {
     it('should respond with a 200', function (done) {
       request
-        .get('/first-time-claim/eligibility/1234567/new-claim')
+        .get(ROUTE)
         .expect(200)
         .end(function (error, response) {
           expect(error).to.be.null
@@ -36,15 +38,12 @@ describe('routes/first-time/eligibility/new-claim/future-or-past-visit', functio
 
   describe('POST /first-time/eligibility/:reference/new-claim', function () {
     it('should redirect to /first-time/eligibility/:reference/new-claim/past', function (done) {
-      // Currently no validation or logic as future claims have not been implemented
-      var reference = '1234567'
-
       request
-        .post(`/first-time-claim/eligibility/${reference}/new-claim`)
+        .post(ROUTE)
         .expect(302)
         .end(function (error, response) {
           expect(error).to.be.null
-          expect(response.header.location).to.equal(`/first-time-claim/eligibility/${reference}/new-claim/past`)
+          expect(response.header.location).to.equal(`${ROUTE}/past`)
           done()
         })
     })
