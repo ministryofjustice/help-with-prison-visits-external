b5c30bbc59760a38c24ef0850f2b3c68c52731df
Thomas Rowe <thomasrowe101@gmail.com>
2016-11-07 11:06:27 +0000

    Updated route, unit test, and HTML.

--- test/unit/routes/first-time/eligibility/claim/test-refreshment-details.js
+++ test/unit/routes/first-time/eligibility/claim/test-refreshment-details.js
@@ -7,7 +7,7 @@ require('sinon-bluebird')
 const ValidationError = require('../../../../../../app/services/errors/validation-error')
 
 describe('routes/first-time/eligibility/claim/light-refreshment-details', function () {
-  const ROUTE = `/first-time-claim/eligibility/A123456/claim/1/refreshment`
+  const ROUTE = `/first-time/eligibility/A123456/claim/1/refreshment`
 
   var app
 
