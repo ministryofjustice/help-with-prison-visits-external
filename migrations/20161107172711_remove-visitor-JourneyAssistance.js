c3052925fd11e686cf39318dcffb8a567b502579
Steven Alexander <steven.william.alexander@googlemail.com>
2016-11-07 17:32:21 +0000

    chore - removed Visitor.JourneyAssistance

---
+++ migrations/20161107172711_remove-visitor-JourneyAssistance.js
@@ -0,0 +1,11 @@
+exports.up = function (knex, Promise) {
+  return knex.schema.table('Visitor', function (table) {
+    table.dropColumn('JourneyAssistance')
+  })
+}
+
+exports.down = function (knex, Promise) {
+  return knex.schema.table('Visitor', function (table) {
+    table.string('JourneyAssistance', 10).notNullable()
+  })
+}
