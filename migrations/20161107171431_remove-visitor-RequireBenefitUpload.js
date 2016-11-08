95bd038cdcf3b32b241da90865e728f02f459a1e
Thomas Rowe <thomasrowe101@gmail.com>
2016-11-08 15:37:07 +0000

    Added migration for adding the isChild column. Fixed a bug in the down function of the Visitor migration.

--- migrations/20161107171431_remove-visitor-RequireBenefitUpload.js
+++ migrations/20161107171431_remove-visitor-RequireBenefitUpload.js
@@ -6,6 +6,6 @@ exports.up = function (knex, Promise) {
 
 exports.down = function (knex, Promise) {
   return knex.schema.table('Visitor', function (table) {
-    table.boolean('RequireBenefitUpload', 10).defaultTo(false).notNullable()
+    table.boolean('RequireBenefitUpload')
   })
 }
