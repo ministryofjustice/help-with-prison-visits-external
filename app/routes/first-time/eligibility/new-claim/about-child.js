5b89520d9f78706941521b2562cb1df80fa2e42d
Thomas Rowe <thomasrowe101@gmail.com>
2016-11-08 17:45:51 +0000

    Added TODOs.

--- app/routes/first-time/eligibility/new-claim/about-child.js
+++ app/routes/first-time/eligibility/new-claim/about-child.js
@@ -1,5 +1,6 @@
 const UrlPathValidator = require('../../../../services/validators/url-path-validator')
 
+// TODO: Add route unit test.
 module.exports = function (router) {
   router.get('/first-time/eligibility/:reference/claim/:claimId/child', function (req, res) {
     UrlPathValidator(req.params)
@@ -13,9 +14,12 @@ module.exports = function (router) {
     UrlPathValidator(req.params)
 
     // TODO: Build a domain object to hold the child details.
+    // TODO: Add unit test for domain object.
     // TODO: Persist the child details domain object.
+    // TODO: Add inegration test for peristance module.
     // TODO: Error handling based on validation.
     // TODO: Need to reload the page if the user selects add another child.
+    // TODO: Update end-to-end test to hit the child page and add two children.
 
     return res.redirect(`/first-time/eligibility/${req.params.reference}/claim/${req.params.claimId}`)
   })
