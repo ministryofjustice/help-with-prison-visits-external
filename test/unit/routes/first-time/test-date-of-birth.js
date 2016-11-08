e728bbb1bf411b71adf67eec35b1f3e7180422c8
Michael Hall <m.hall@kainos.com>
2016-11-08 12:47:28 +0000

    Added domain object and test for date of birth and refactored test (#138)
    Added domain object and test for date of birth and refactored test
    
    * Added domain object and test
    * Removed old validator
    * Refactored route test

--- test/unit/routes/first-time/test-date-of-birth.js
+++ test/unit/routes/first-time/test-date-of-birth.js
@@ -1,73 +1,67 @@
-var supertest = require('supertest')
-var proxyquire = require('proxyquire')
-var express = require('express')
-var mockViewEngine = require('../mock-view-engine')
-var bodyParser = require('body-parser')
-var moment = require('moment')
-const dateFormatter = require('../../../../app/services/date-formatter')
+const routeHelper = require('../../../helpers/routes/route-helper')
+const supertest = require('supertest')
+const proxyquire = require('proxyquire')
+const sinon = require('sinon')
+require('sinon-bluebird')
+const ValidationError = require('../../../../app/services/errors/validation-error')
 
-var validationErrors
-
-var route = proxyquire(
-  '../../../../app/routes/first-time/new-eligibility/date-of-birth', {
-    '../../../services/validators/eligibility/date-of-birth-validator': function () { return validationErrors }
-  })
+var stubDateOfBirth
+var app
 
 describe('routes/first-time/new-eligibility/date-of-birth', function () {
   const ROUTE = '/first-time/new-eligibility'
 
-  var request
-  var dobDay = '01'
-  var dobMonth = '05'
-  var dobYear = '1955'
-  var dob = dateFormatter.buildFormatted(dobDay, dobMonth, dobYear)
-
   beforeEach(function () {
-    var app = express()
-    app.use(bodyParser.json())
-    mockViewEngine(app, '../../../app/views')
-    route(app)
-    request = supertest(app)
-    validationErrors = false
+    stubDateOfBirth = sinon.stub()
+
+    var route = proxyquire(
+      '../../../../app/routes/first-time/new-eligibility/date-of-birth', {
+        '../../../services/domain/date-of-birth': stubDateOfBirth
+      })
+
+    app = routeHelper.buildApp(route)
   })
 
   describe(`GET ${ROUTE}`, function () {
-    it('should respond with a 200', function (done) {
-      request
+    it('should respond with a 200', function () {
+      return supertest(app)
         .get(ROUTE)
         .expect(200)
-        .end(done)
     })
   })
 
   describe(`POST ${ROUTE}`, function () {
     describe('for over-sixteen date', function () {
-      it('should respond with a 302 and redirect /first-time/:dob', function (done) {
-        request
+      it('should respond with a 302 and redirect /first-time/new-eligibility/:dob', function () {
+        var dob = '1980-10-10'
+        stubDateOfBirth.returns({getDobFormatted: dob, sixteenOrUnder: false})
+        return supertest(app)
           .post(ROUTE)
-          .send({
-            'dob-day': dobDay,
-            'dob-month': dobMonth,
-            'dob-year': dobYear
-          })
           .expect(302)
-          .expect('location', `${ROUTE}/${dob}`)
-          .end(done)
+          .expect(function () {
+            sinon.assert.calledOnce(stubDateOfBirth)
+          })
+          .expect('location', `/first-time/new-eligibility/${dob}`)
+      })
+
+      it('should respond with a 400 for invalid data', function () {
+        stubDateOfBirth.throws(new ValidationError({ 'dob': {} }))
+        return supertest(app)
+          .post(ROUTE)
+          .expect(400)
+          .expect(function () {
+            sinon.assert.calledOnce(stubDateOfBirth)
+          })
       })
     })
 
     describe('sixteen or under date', function () {
-      it('should respond with a 302 and redirect to /eligibility-fail', function (done) {
-        request
+      it('should respond with a 302 and redirect to /eligibility-fail', function () {
+        stubDateOfBirth.returns({sixteenOrUnder: true})
+        return supertest(app)
           .post(ROUTE)
-          .send({
-            'dob-day': '1',
-            'dob-month': '1',
-            'dob-year': moment().year() - 16
-          })
           .expect(302)
           .expect('location', '/eligibility-fail')
-          .end(done)
       })
     })
   })
