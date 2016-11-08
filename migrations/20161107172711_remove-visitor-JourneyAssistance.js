c70d3200b3e212e30db5aff759cf270ba259b3f7
Steven Alexander <steven.william.alexander@googlemail.com>
2016-11-08 09:23:17 +0000

    corrected rollback on migrations

--- migrations/20161107172711_remove-visitor-JourneyAssistance.js
+++ migrations/20161107172711_remove-visitor-JourneyAssistance.js
@@ -6,6 +6,6 @@ exports.up = function (knex, Promise) {
 
 exports.down = function (knex, Promise) {
   return knex.schema.table('Visitor', function (table) {
-    table.string('JourneyAssistance', 10).notNullable()
+    table.string('JourneyAssistance', 10).defaultTo('no').notNullable()
   })
 }
