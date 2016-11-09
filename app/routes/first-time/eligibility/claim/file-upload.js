d075179d77c651d65336132c9546863d690d1883
Michael Hall <m.hall@kainos.com>
2016-11-09 15:22:45 +0000

    Feature/apvs 672/file upload with guiding text (#149)
    Apvs 672 - File upload page with guiding text
    
    Updated file upload page to latest UX
    Added enum for guiding text
    Updated link to upload to take query
    Check on query to make sure valid
    File upload test updated

--- app/routes/first-time/eligibility/claim/file-upload.js
+++ app/routes/first-time/eligibility/claim/file-upload.js
@@ -1,8 +1,17 @@
 const UrlPathValidator = require('../../../../services/validators/url-path-validator')
+const fileUploadGuidingText = require('../../../../constants/file-upload-guiding-text-enum')
+const documentTypeEnum = require('../../../../constants/document-type-enum')
 
 module.exports = function (router) {
   router.get('/first-time/eligibility/:reference/claim/:claimId/file-upload', function (req, res) {
     UrlPathValidator(req.params)
-    res.render('first-time/eligibility/claim/file-upload', {})
+    if (documentTypeEnum.hasOwnProperty(req.query.document)) {
+      res.render('first-time/eligibility/claim/file-upload', {
+        document: req.query.document,
+        fileUploadGuidingText: fileUploadGuidingText
+      })
+    } else {
+      throw new Error('Not a valid document type')
+    }
   })
 }
