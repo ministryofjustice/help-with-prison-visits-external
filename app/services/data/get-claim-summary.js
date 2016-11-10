b4baaa8b33946897da1df0d9b92b2c9bf7085232
Thomas Rowe <thomasrowe101@gmail.com>
2016-11-10 12:23:28 +0000

    Added HTML to display child details. Cleaned up get module. Added TODOs

--- app/services/data/get-claim-summary.js
+++ app/services/data/get-claim-summary.js
@@ -2,26 +2,46 @@ const config = require('../../../knexfile').extweb
 const knex = require('knex')(config)
 const documentTypeEnum = require('../../constants/document-type-enum')
 
+// TODO: Need to add ability to retrieve claimChild table contents.
 module.exports = function (claimId) {
   return knex('Claim')
     .join('Eligibility', 'Claim.Reference', '=', 'Eligibility.Reference')
     .join('Visitor', 'Eligibility.Reference', '=', 'Visitor.Reference')
     .join('Prisoner', 'Eligibility.Reference', '=', 'Prisoner.Reference')
     .where('Claim.ClaimId', claimId)
-    .first('Eligibility.Reference', 'Claim.DateSubmitted', 'Claim.DateOfJourney', 'Visitor.FirstName', 'Visitor.LastName', 'Visitor.Benefit',
-      'Prisoner.FirstName AS PrisonerFirstName', 'Prisoner.LastName AS PrisonerLastName',
-      'Prisoner.DateOfBirth AS PrisonerDateOfBirth', 'Prisoner.PrisonNumber', 'Prisoner.NameOfPrison')
+    .first(
+      'Eligibility.Reference',
+      'Claim.DateSubmitted',
+      'Claim.DateOfJourney',
+      'Visitor.FirstName',
+      'Visitor.LastName',
+      'Visitor.Benefit',
+      'Prisoner.FirstName AS PrisonerFirstName',
+      'Prisoner.LastName AS PrisonerLastName',
+      'Prisoner.DateOfBirth AS PrisonerDateOfBirth',
+      'Prisoner.PrisonNumber',
+      'Prisoner.NameOfPrison'
+    )
     .then(function (claim) {
       return knex('ClaimDocument')
         .join('Claim', 'ClaimDocument.ClaimId', '=', 'Claim.ClaimId')
-        .where({'ClaimDocument.DocumentType': documentTypeEnum.VISIT_CONFIRMATION, 'Claim.ClaimId': claimId})
+        .where({ 'ClaimDocument.DocumentType': documentTypeEnum.VISIT_CONFIRMATION, 'Claim.ClaimId': claimId} )
         .first('ClaimDocument.DocumentStatus', 'ClaimDocument.DocumentType')
         .then(function (visitConfirmationDocumentStatus) {
           return knex('Claim')
             .join('ClaimExpense', 'Claim.ClaimId', '=', 'ClaimExpense.ClaimId')
-            .where({'Claim.ClaimId': claimId, 'ClaimExpense.IsEnabled': true})
-            .select('ClaimExpense.ClaimExpenseId', 'ClaimExpense.ExpenseType', 'ClaimExpense.Cost', 'ClaimExpense.To', 'ClaimExpense.From', 'ClaimExpense.IsReturn', 'ClaimExpense.TravelTime',
-              'ClaimExpense.DurationOfTravel', 'ClaimExpense.TicketType')
+            .where({ 'Claim.ClaimId': claimId, 'ClaimExpense.IsEnabled': true })
+            .select(
+              'ClaimExpense.ClaimExpenseId',
+              'ClaimExpense.ExpenseType',
+              'ClaimExpense.Cost',
+              'ClaimExpense.To',
+              'ClaimExpense.From',
+              'ClaimExpense.IsReturn',
+              'ClaimExpense.TravelTime',
+              'ClaimExpense.DurationOfTravel',
+              'ClaimExpense.TicketType'
+            )
             .orderBy('ClaimExpense.ClaimExpenseId')
             .then(function (claimExpenses) {
               claimExpenses.forEach(function (expense) {
