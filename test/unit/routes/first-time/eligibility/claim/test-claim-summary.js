27e787ed180351dc9e9a4590a146d20d0e26ea34
Michael Hall <m.hall@kainos.com>
2016-11-14 17:14:02 +0000

    Added tests to route for remove file

--- test/unit/routes/first-time/eligibility/claim/test-claim-summary.js
+++ test/unit/routes/first-time/eligibility/claim/test-claim-summary.js
@@ -12,22 +12,26 @@ const ELIGIBILITYID = '1234'
 const REFERENCEID = `${REFERENCE}-${ELIGIBILITYID}`
 const CLAIMID = '1'
 const CLAIMEXPENSEID = '1234'
+const CLAIMDOCUMENTDETAILS = {documentType: 'VISIT_CONFIRMATION'}
 
 describe('routes/first-time/eligibility/claim/claim-summary', function () {
   var request
   var urlValidatorCalled
   var getClaimSummary
   var removeClaimExpense
+  var removeClaimDocument
 
   beforeEach(function () {
     getClaimSummary = sinon.stub().resolves()
     removeClaimExpense = sinon.stub().resolves()
+    removeClaimDocument = sinon.stub().resolves()
 
     var route = proxyquire(
       '../../../../../../app/routes/first-time/eligibility/claim/claim-summary', {
         '../../../../services/validators/url-path-validator': function () { urlValidatorCalled = true },
         '../../../../services/data/get-claim-summary': getClaimSummary,
-        '../../../../services/data/remove-claim-expense': removeClaimExpense
+        '../../../../services/data/remove-claim-expense': removeClaimExpense,
+        '../../../../services/data/remove-claim-document': removeClaimDocument
       })
 
     var app = express()
@@ -75,6 +79,21 @@ describe('routes/first-time/eligibility/claim/claim-summary', function () {
           expect(error).to.be.null
           expect(urlValidatorCalled).to.be.true
           expect(removeClaimExpense.calledWith(CLAIMID, CLAIMEXPENSEID)).to.be.true
+          expect(response.headers['location']).to.be.equal(`/first-time/eligibility/${REFERENCEID}/claim/${CLAIMID}/summary`)
+          done()
+        })
+    })
+  })
+
+  describe('POST /first-time/eligibility/:referenceId/claim/:claimId/summary/removeFile', function () {
+    it('should respond with a 302 and call removeClaimDocument', function (done) {
+      request
+        .post(`/first-time/eligibility/${REFERENCEID}/claim/${CLAIMID}/summary/removeFile?documentType=VISIT_CONFIRMATION`)
+        .expect(302)
+        .end(function (error, response) {
+          expect(error).to.be.null
+          expect(urlValidatorCalled).to.be.true
+          expect(removeClaimDocument.calledWith(CLAIMID, CLAIMDOCUMENTDETAILS)).to.be.true
           expect(response.headers['location']).to.be.equal(`/first-time/eligibility/${REFERENCEID}/claim/${CLAIMID}/summary`)
           done()
         })
