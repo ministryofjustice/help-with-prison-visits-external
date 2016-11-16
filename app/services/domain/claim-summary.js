62bec5de8bbb1877c206cc572c43ae88fbde78c3
Michael Hall <m.hall@kainos.com>
2016-11-16 12:39:09 +0000

    Feature/apvs-348: Validation for benefit document (#185)
    Added validation for benefit document being uploaded if needed
    
    Updated domain for claim summary
    Updated domain test
    Updated route
    Updated route test
    Added warning flag and id into html

--- app/services/domain/claim-summary.js
+++ app/services/domain/claim-summary.js
@@ -1,15 +1,23 @@
 const ValidationError = require('../errors/validation-error')
 const FieldValidator = require('../validators/field-validator')
 const ErrorHandler = require('../validators/error-handler')
+const BenefitEnum = require('../../constants/benefits-enum')
 
 class ClaimSummary {
-  constructor (visitConfirmation) {
+  constructor (visitConfirmation, benefit, benefitDocument) {
+    this.benefit = benefit
+    this.benefitDocumentStatus = benefitDocument ? benefitDocument.DocumentStatus : ''
     this.visitConfirmationStatus = visitConfirmation ? visitConfirmation.DocumentStatus : ''
     this.IsValid()
   }
 
   IsValid () {
     var errors = ErrorHandler()
+
+    if (BenefitEnum[this.benefit].requireBenefitUpload) {
+      FieldValidator(this.benefitDocumentStatus, 'benefit-information', errors)
+        .isRequired()
+    }
 
     FieldValidator(this.visitConfirmationStatus, 'VisitConfirmation', errors)
       .isRequired()
