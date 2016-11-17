0641a22e58980f5e8ccb764a716441cbe44f8f89
Michael Hall <m.hall@kainos.com>
2016-11-17 11:18:47 +0000

    Calling remove file when remove expense is called

--- app/routes/first-time/eligibility/claim/claim-summary.js
+++ app/routes/first-time/eligibility/claim/claim-summary.js
@@ -86,7 +86,10 @@ module.exports = function (router) {
 
     removeClaimExpense(req.params.claimId, req.params.claimExpenseId)
       .then(function () {
-        return res.redirect(`/first-time/eligibility/${req.params.referenceId}/claim/${req.params.claimId}/summary`)
+        removeClaimDocument(req.query.claimDocumentId)
+          .then(function () {
+            return res.redirect(`/first-time/eligibility/${req.params.referenceId}/claim/${req.params.claimId}/summary`)
+          })
       })
       .catch(function (error) {
         next(error)
