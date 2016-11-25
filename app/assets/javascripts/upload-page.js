313f983eb2daa32f1e7697f86cb6afe2c554bc46
Michael Hall <m.hall@kainos.com>
2016-11-25 16:57:48 +0000

    Ux changes for making file upload look better (#230)

--- app/assets/javascripts/upload-page.js
+++ app/assets/javascripts/upload-page.js
@@ -2,7 +2,9 @@ $('#document').change(function () {
   var document = $('#document').val().replace(/\\/g, '/').replace(/.*\//, '')
   if (document) {
     $('#alternative').hide()
-    $('#document-name').html(document)
-    $('#choose-file').html('Choose a different file')
+    $('#document-name').html(document).addClass('text-success')
+    $('#choose-file').hide()
+    $('#label').hide()
+    $('#remove-file-upload').show()
   }
 })
