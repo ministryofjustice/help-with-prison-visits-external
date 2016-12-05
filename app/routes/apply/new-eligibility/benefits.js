c0966567d4504f8f8d40071c76d47cdd6de6c8be
Steven Alexander <steven.william.alexander@googlemail.com>
2016-12-05 15:58:20 +0000

    bug - benefit screen was displaying wrong view on validation error

--- app/routes/apply/new-eligibility/benefits.js
+++ app/routes/apply/new-eligibility/benefits.js
@@ -32,7 +32,7 @@ module.exports = function (router) {
       }
     } catch (error) {
       if (error instanceof ValidationError) {
-        return res.status(400).render('apply/new-eligibility/prisoner-relationship', {
+        return res.status(400).render('apply/new-eligibility/benefits', {
           errors: error.validationErrors,
           URL: req.url
         })
