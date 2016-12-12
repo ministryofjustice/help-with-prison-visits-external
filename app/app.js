7c1a2115dd2e16be47888c3fbf14ab4536cab8f6
Thomas Swann <ts.swann@gmail.com>
2016-12-12 11:19:45 +0000

    Configure content security policy settings

--- app/app.js
+++ app/app.js
@@ -24,6 +24,21 @@ app.use(compression())
 // Set security headers.
 app.use(helmet())
 
+// Configure Content Security Policy
+// Hashes for inline Gov Template script entries
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'",
+      'www.google-analytics.com',
+      "'sha256-+6WnXIl4mbFTCARd8N3COQmT3bJJmo32N8q8ZSQAIcU='",
+      "'sha256-G29/qSW/JHHANtFhlrZVDZW1HOkCDRc78ggbqwwIJ2g='"],
+    styleSrc: ["'self'"],
+    fontSrc: ['data:'],
+    imgSrc: ["'self'", 'www.google-analytics.com']
+  }
+}))
+
 var packageJson = require('../package.json')
 var developmentMode = app.get('env') === 'development'
 var releaseVersion = packageJson.version
