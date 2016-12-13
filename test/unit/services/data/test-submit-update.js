9fcec4a23f487b7b34e2e13eacbc9cc01f9b779b
Thomas Swann <ts.swann@gmail.com>
2016-12-13 15:41:42 +0000

    Updating integration tests for submit update claim

--- test/unit/services/data/test-submit-update.js
+++ test/unit/services/data/test-submit-update.js
@@ -3,16 +3,21 @@ const proxyquire = require('proxyquire')
 const sinon = require('sinon')
 require('sinon-bluebird')
 const tasksEnum = require('../../../../app/constants/tasks-enum')
+const BankAccountDetails = require('../../../../app/services/domain/bank-account-details')
 
 const REPEAT_ELIGIBILITY = {EmailAddress: 'test@test.com'}
 const REFERENCE = 'V123456'
 const ELIGIBILITYID = 1
 const CLAIMID = 1
+const BANK_DETAILS = { sortCode: 112233, accountNumber: 33445566, required: true }
+const TEST_BANK_ACCOUNT_DETAILS = new BankAccountDetails(112233, 33445566)
+const NO_BANK_DETAILS = { required: false }
 
 describe('services/data/submit-update', function () {
   var submitUpdate
   var getRepeatEligibilityStub
   var insertTaskStub
+  var insertBankDetailsStub
 
   before(function () {
     getRepeatEligibilityStub = sinon.stub()
@@ -20,25 +25,39 @@ describe('services/data/submit-update', function () {
 
     submitUpdate = proxyquire('../../../../app/services/data/submit-update', {
       './get-repeat-eligibility': getRepeatEligibilityStub,
-      './insert-task': insertTaskStub
+      './insert-task': insertTaskStub,
+      './insert-bank-account-details-for-claim': insertBankDetailsStub
     })
   })
 
   it('should insert two tasks and retrieve email address', function () {
     getRepeatEligibilityStub.resolves(REPEAT_ELIGIBILITY)
     insertTaskStub.resolves()
-    return submitUpdate(REFERENCE, ELIGIBILITYID, CLAIMID, '', { required: false })
+    return submitUpdate(REFERENCE, ELIGIBILITYID, CLAIMID, '', NO_BANK_DETAILS)
       .then(function () {
         expect(insertTaskStub.calledWith(REFERENCE, ELIGIBILITYID, CLAIMID, tasksEnum.REQUEST_INFORMATION_RESPONSE, '')).to.be.true
         expect(getRepeatEligibilityStub.calledWith(REFERENCE, null, ELIGIBILITYID)).to.be.true
         expect(insertTaskStub.calledWith(REFERENCE, ELIGIBILITYID, CLAIMID, tasksEnum.REQUEST_INFORMATION_RESPONSE_SUBMITTED_NOTIFICATION, REPEAT_ELIGIBILITY.EmailAddress)).to.be.true
+        expect(insertBankDetailsStub.calledOnce).to.be.false
+      })
+  })
+
+  it('should insert bank details if they have been updated', function () {
+    getRepeatEligibilityStub.resolves(REPEAT_ELIGIBILITY)
+    insertTaskStub.resolves()
+    return submitUpdate(REFERENCE, ELIGIBILITYID, CLAIMID, '', BANK_DETAILS)
+      .then(function () {
+        expect(insertTaskStub.calledWith(REFERENCE, ELIGIBILITYID, CLAIMID, tasksEnum.REQUEST_INFORMATION_RESPONSE, '')).to.be.true
+        expect(getRepeatEligibilityStub.calledWith(REFERENCE, null, ELIGIBILITYID)).to.be.true
+        expect(insertTaskStub.calledWith(REFERENCE, ELIGIBILITYID, CLAIMID, tasksEnum.REQUEST_INFORMATION_RESPONSE_SUBMITTED_NOTIFICATION, REPEAT_ELIGIBILITY.EmailAddress)).to.be.true
+        expect(insertBankDetailsStub.calledWith(REFERENCE, ELIGIBILITYID, CLAIMID, TEST_BANK_ACCOUNT_DETAILS)).to.be.true
       })
   })
 
   it('should throw an error when no email address exists', function () {
     getRepeatEligibilityStub.resolves({})
     insertTaskStub.resolves()
-    return submitUpdate(REFERENCE, ELIGIBILITYID, CLAIMID, '', { required: false })
+    return submitUpdate(REFERENCE, ELIGIBILITYID, CLAIMID, '', NO_BANK_DETAILS)
       .then(function () {
         expect(insertTaskStub.calledWith(REFERENCE, ELIGIBILITYID, CLAIMID, tasksEnum.REQUEST_INFORMATION_RESPONSE, '')).to.be.true
       })
