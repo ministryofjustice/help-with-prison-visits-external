c6028866ca0341b28da2b1f2f85a9c35721a9df4
Thomas Swann <ts.swann@gmail.com>
2017-01-19 15:11:38 +0000

    Clear up malware test file instructions

--- test/integration/services/test-clam-av.js
+++ test/integration/services/test-clam-av.js
@@ -4,22 +4,20 @@ const clam = require('../../../app/services/clam-av')
 // Include full path to test files for Clam AV Daemon when running locally
 // Also, you will need to edit the contents of test_malware.txt to make the
 // contents appear as a virus. Full instructions included in the file itself.
-const MALWARE_FILE_PATH = '<INCLUDE FULL PATH>/resources/test_malware.txt'
-const VALID_FILE_PATH = '<INCLUDE FULL PATH>/resources/testfile.txt'
+const MALWARE_FILE_PATH = '<INCLUDE FULL PATH>/test/resources/test_malware.txt'
+const VALID_FILE_PATH = '<INCLUDE FULL PATH>/test/resources/testfile.txt'
 
 describe('services/clam-av anti-virus check on file upload', function () {
-  it('should detect a file with malicious content', function (done) {
-    clam.scan(MALWARE_FILE_PATH).then((infected) => {
+  it('should detect a file with malicious content', function () {
+    return clam.scan(MALWARE_FILE_PATH).then((infected) => {
       expect(infected).to.equal(true)
     })
-    done()
   })
 
-  it('should pass a file with no malicious content', function (done) {
-    clam.scan(VALID_FILE_PATH).then((infected) => {
+  it('should pass a file with no malicious content', function () {
+    return clam.scan(VALID_FILE_PATH).then((infected) => {
       expect(infected).to.equal(false)
     })
-    done()
   })
 })
 */
