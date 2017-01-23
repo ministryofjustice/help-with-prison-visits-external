99256446a904c823c084be4067f68d942b03bfba
Michael Hall <m.hall@kainos.com>
2017-01-23 11:04:41 +0000

    Chore/alter claim refence and eligibilityid fields to fix issue (#368)
    * Removed unique constraint on reference and eligibility id of claim
    
    * Added comments to explain removal of foreign key

--- migrations/20161221121204_add-claim-escort.js
+++ migrations/20161221121204_add-claim-escort.js
@@ -1,8 +1,8 @@
 exports.up = function (knex, Promise) {
   return knex.schema.createTable('ClaimEscort', function (table) {
     table.increments('ClaimEscortId')
-    table.integer('EligibilityId').unsigned().notNullable().references('Claim.EligibilityId')
-    table.string('Reference', 10).notNullable().index().references('Claim.Reference')
+    table.integer('EligibilityId').unsigned().notNullable().references('Claim.EligibilityId') // REMOVED FOREIGN KEY IN LATER MIGRATION
+    table.string('Reference', 10).notNullable().index().references('Claim.Reference') // REMOVED FOREIGN KEY IN LATER MIGRATION
     table.integer('ClaimId').unsigned().notNullable().references('Claim.ClaimId')
     table.string('FirstName', 100).notNullable()
     table.string('LastName', 100).notNullable()
