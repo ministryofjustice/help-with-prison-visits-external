813be9f582664e3591c9c08b7c2a4fdef9b0317f
Michael Hall <m.hall@kainos.com>
2017-01-25 15:36:30 +0000

    Updated domain test of test-bank-account-details

--- test/unit/services/domain/test-bank-account-details.js
+++ test/unit/services/domain/test-bank-account-details.js
@@ -10,65 +10,46 @@ describe('services/domain/bank-account-details', function () {
   const PROCESSED_ACCOUNT_NUMBER = '12345678'
   const PROCESSED_SORT_CODE = '123456'
 
-  it('should construct a domain object given valid input', function (done) {
+  it('should construct a domain object given valid input', function () {
     bankAccountDetails = new BankAccountDetails(VALID_ACCOUNT_NUMBER, VALID_SORT_CODE, VALID_TERMS_AND_CONDITIONS)
     expect(bankAccountDetails.accountNumber).to.equal(PROCESSED_ACCOUNT_NUMBER)
     expect(bankAccountDetails.sortCode).to.equal(PROCESSED_SORT_CODE)
     expect(bankAccountDetails.termsAndConiditions).to.equal(VALID_TERMS_AND_CONDITIONS)
-    done()
   })
 
-  it('should construct a domain object given a sort code with hyphens', function (done) {
+  it('should construct a domain object given a sort code with hyphens', function () {
     var sortCodeWithHyphens = '12-12-12'
     var processedSortCodeWithHyphens = '121212'
     bankAccountDetails = new BankAccountDetails(VALID_ACCOUNT_NUMBER, sortCodeWithHyphens, VALID_TERMS_AND_CONDITIONS)
     expect(bankAccountDetails.accountNumber).to.equal(PROCESSED_ACCOUNT_NUMBER)
     expect(bankAccountDetails.sortCode).to.equal(processedSortCodeWithHyphens)
     expect(bankAccountDetails.termsAndConiditions).to.equal(VALID_TERMS_AND_CONDITIONS)
-    done()
   })
 
-  it('should construct a domain object given a sort code with hyphens and spaces', function (done) {
+  it('should construct a domain object given a sort code with hyphens and spaces', function () {
     var sortCodeWithHyphensAndSpaces = '12 - 12 - 12'
     var processedSortCodeWithHyphensAndSpaces = '121212'
     bankAccountDetails = new BankAccountDetails(VALID_ACCOUNT_NUMBER, sortCodeWithHyphensAndSpaces, VALID_TERMS_AND_CONDITIONS)
     expect(bankAccountDetails.accountNumber).to.equal(PROCESSED_ACCOUNT_NUMBER)
     expect(bankAccountDetails.sortCode).to.equal(processedSortCodeWithHyphensAndSpaces)
     expect(bankAccountDetails.termsAndConiditions).to.equal(VALID_TERMS_AND_CONDITIONS)
-    done()
   })
 
-  it('should return isRequired errors given empty strings', function (done) {
-    try {
-      bankAccountDetails = new BankAccountDetails('', '', '')
-    } catch (e) {
-      expect(e).to.be.instanceof(ValidationError)
-      expect(e.validationErrors['AccountNumber'][0]).to.equal('Account number is required')
-      expect(e.validationErrors['SortCode'][0]).to.equal('Sort code is required')
-      expect(e.validationErrors['terms-and-conditions'][0]).to.equal('Agreement to our terms and conditions is required')
-    }
-    done()
+  it('should return isRequired errors given empty strings', function () {
+    expect(function () {
+      new BankAccountDetails('', '', '').isValid()
+    }).to.throw(ValidationError)
   })
 
-  it('should return isNumber errors given letters', function (done) {
-    try {
-      bankAccountDetails = new BankAccountDetails('asdf', 'asdf', VALID_TERMS_AND_CONDITIONS)
-    } catch (e) {
-      expect(e).to.be.instanceof(ValidationError)
-      expect(e.validationErrors['AccountNumber'][0]).to.equal('Account number must only contain numbers')
-      expect(e.validationErrors['SortCode'][0]).to.equal('Sort code must only contain numbers')
-    }
-    done()
+  it('should return isNumber errors given letters', function () {
+    expect(function () {
+      new BankAccountDetails('asdf', 'asdf', VALID_TERMS_AND_CONDITIONS).isValid()
+    }).to.throw(ValidationError)
   })
 
-  it('should return isLength errors given invalid length', function (done) {
-    try {
-      bankAccountDetails = new BankAccountDetails('123456789', '123', VALID_TERMS_AND_CONDITIONS)
-    } catch (e) {
-      expect(e).to.be.instanceof(ValidationError)
-      expect(e.validationErrors['AccountNumber'][0]).to.equal('Account number must be 8 characters in length')
-      expect(e.validationErrors['SortCode'][0]).to.equal('Sort code must be 6 characters in length')
-    }
-    done()
+  it('should return isLength errors given invalid length', function () {
+    expect(function () {
+      new BankAccountDetails('123456789', '123', VALID_TERMS_AND_CONDITIONS).isValid()
+    }).to.throw(ValidationError)
   })
 })
