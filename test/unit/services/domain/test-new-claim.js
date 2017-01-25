094b1d9d753b5a78fa2645822a212163661ce83a
Michael Hall <m.hall@kainos.com>
2017-01-25 15:27:33 +0000

    Updated test-new-claim domain test

--- test/unit/services/domain/test-new-claim.js
+++ test/unit/services/domain/test-new-claim.js
@@ -33,14 +33,9 @@ describe('services/domain/new-claim', function () {
   })
 
   it('should return isRequired errors given empty strings', function () {
-    try {
-      claim = new NewClaim('', '', '', '', false)
-      expect(false, 'should have throw validation error').to.be.true
-    } catch (e) {
-      expect(e).to.be.instanceof(ValidationError)
-      expect(e.validationErrors['Reference'][0]).to.equal('Reference is required')
-      expect(e.validationErrors['DateOfJourney'][0]).to.equal('Date of prison visit was invalid')
-    }
+    expect(function () {
+      new NewClaim('', '', '', '', false).isValid()
+    }).to.throw(ValidationError)
   })
 
   it('should not return isRequired errors for child-visitor if is repeat duplicate claim', function () {
@@ -49,37 +44,37 @@ describe('services/domain/new-claim', function () {
   })
 
   it('should return isValidDate error given an invalid type for date', function () {
-    try {
-      claim = new NewClaim(VALID_REFERENCE, 'invalid day', 'invalid month', 'invalid year', '', false, VALID_IS_ADVANCE_CLAIM)
-      expect(false, 'should have throw validation error').to.be.true
-    } catch (e) {
-      expect(e).to.be.instanceof(ValidationError)
-      expect(e.validationErrors['DateOfJourney'][0]).to.equal('Date of prison visit was invalid')
-    }
+    expect(function () {
+      new NewClaim(
+        VALID_REFERENCE,
+        'invalid day',
+        'invalid month',
+        'invalid year',
+        '',
+        false,
+        VALID_IS_ADVANCE_CLAIM
+      ).isValid()
+    }).to.throw(ValidationError)
   })
 
   it('should return isValidDate error given a date in the future for non-advance claims', function () {
-    try {
-      var futureDate = dateFormatter.now().add(1, 'days')
-      claim = new NewClaim(
+    var futureDate = dateFormatter.now().add(1, 'days')
+    expect(function () {
+      new NewClaim(
         VALID_REFERENCE,
         futureDate.date(),
         futureDate.month() + 1,
         futureDate.year(),
         VALID_IS_ADVANCE_CLAIM
-      )
-      expect(false, 'should have throw validation error').to.be.true
-    } catch (e) {
-      expect(e).to.be.instanceof(ValidationError)
-      expect(e.validationErrors['DateOfJourney'][0]).to.equal('Date of prison visit must be in the past')
-    }
+      ).isValid()
+    }).to.throw(ValidationError)
   })
 
   it('should return isValidDate error given a date in the future for advance claims', function () {
-    try {
-      var isAdvanceClaim = true
-      var pastDate = dateFormatter.now().add(-1)
-      claim = new NewClaim(
+    var isAdvanceClaim = true
+    var pastDate = dateFormatter.now().add(-1)
+    expect(function () {
+      new NewClaim(
         VALID_REFERENCE,
         pastDate.date(),
         pastDate.month() + 1,
@@ -87,19 +82,15 @@ describe('services/domain/new-claim', function () {
         VALID_CHILD_VISITOR,
         false,
         isAdvanceClaim
-      )
-      expect(false, 'should have throw validation error').to.be.true
-    } catch (e) {
-      expect(e).to.be.instanceof(ValidationError)
-      expect(e.validationErrors['DateOfJourney'][0]).to.equal('Date of prison visit must be in the future')
-    }
+      ).isValid()
+    }).to.throw(ValidationError)
   })
 
   it('should return isValidDate error given a date less than 5 days away for advance claims', function () {
-    try {
-      var isAdvanceClaim = true
-      var futureDate = dateFormatter.now().add(4, 'days')
-      claim = new NewClaim(
+    var isAdvanceClaim = true
+    var futureDate = dateFormatter.now().add(4, 'days')
+    expect(function () {
+      new NewClaim(
         VALID_REFERENCE,
         futureDate.date(),
         futureDate.month() + 1,
@@ -107,28 +98,20 @@ describe('services/domain/new-claim', function () {
         VALID_CHILD_VISITOR,
         false,
         isAdvanceClaim
-      )
-      expect(false, 'should have throw validation error').to.be.true
-    } catch (e) {
-      expect(e).to.be.instanceof(ValidationError)
-      expect(e.validationErrors['DateOfJourney'][0]).to.equal('Date of prison visit must not be within 5 days')
-    }
+      ).isValid()
+    }).to.throw(ValidationError)
   })
 
   it('should return isDateWithinDays error given a date more than 28 days away', function () {
-    try {
-      var dateFurtherThan28Days = dateFormatter.now().subtract(29, 'days')
-      claim = new NewClaim(
+    var dateFurtherThan28Days = dateFormatter.now().subtract(29, 'days')
+    expect(function () {
+      new NewClaim(
         VALID_REFERENCE,
         dateFurtherThan28Days.date(),
         dateFurtherThan28Days.month() + 1,
         dateFurtherThan28Days.year(),
         VALID_IS_ADVANCE_CLAIM
-      )
-      expect(false, 'should have throw validation error').to.be.true
-    } catch (e) {
-      expect(e).to.be.instanceof(ValidationError)
-      expect(e.validationErrors['DateOfJourney'][0]).to.equal('Date of prison visit must be within 28 days')
-    }
+      ).isValid()
+    }).to.throw(ValidationError)
   })
 })
