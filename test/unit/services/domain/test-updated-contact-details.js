dcf42097fb9d156f26c77ad6f96a94e449199d54
Thomas Rowe <thomasrowe101@gmail.com>
2017-01-27 14:32:12 +0000

    Added tests to cover the case where no inputs, or null/undefined inputs are passed to the domain objects and the default values are used. (#405)
    Brings test coverage up to 100% for all updated files.

--- test/unit/services/domain/test-updated-contact-details.js
+++ test/unit/services/domain/test-updated-contact-details.js
@@ -1,16 +1,21 @@
+/* eslint-disable no-new */
 const UpdatedContactDetails = require('../../../../app/services/domain/updated-contact-details')
 const ValidationError = require('../../../../app/services/errors/validation-error')
 const expect = require('chai').expect
 
 describe('services/domain/update-contact-details', function () {
+  const VALID_EMAIL = 'test@test.com'
+  const VALID_PHONE_NUMBER = '02121 565 654'
+
   it('should construct a domain object given valid input', function () {
-    var updatedContactDetails = new UpdatedContactDetails('test@test.com', '')
-    expect(updatedContactDetails.emailAddress).to.equal('test@test.com')
+    var updatedContactDetails = new UpdatedContactDetails(VALID_EMAIL, VALID_PHONE_NUMBER)
+    expect(updatedContactDetails.emailAddress).to.equal(VALID_EMAIL)
+    expect(updatedContactDetails.phoneNumber).to.equal(VALID_PHONE_NUMBER)
   })
 
-  it('should throw an error if passed invalid data', function () {
+  it('should throw a validation error if any inputs were not set and the default domain object values where used', function () {
     expect(function () {
-      new UpdatedContactDetails('', '').isValid()
+      new UpdatedContactDetails()
     }).to.throw(ValidationError)
   })
 })
