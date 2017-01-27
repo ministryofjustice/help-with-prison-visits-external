88c71ace927d67eee2564cf7d990c5828c39833f
Gary Douglas <garyd@kainos.com>
2017-01-27 15:03:21 +0000

    Reverted changes to wdio.conf.sauce.test

--- test/wdio.conf.sauce.test.js
+++ test/wdio.conf.sauce.test.js
@@ -7,13 +7,8 @@ exports.config = merge(wdioConf.config, {
   key: process.env.SAUCE_ACCESS_KEY,
   baseUrl: process.env.EXT_WEB_TEST_BASEURL || 'http://localhost:3000',
   capabilities: [{
-    platformName: 'iOS',
-    platformVersion: '9.1',
-    browserName: 'Safari',
-    deviceName: 'iPhone 6s Simulator',
-    deviceOrientation: 'portrait'
-  }],
-  browserName: 'firefox',
-  platform: 'Windows 10',
-  version: '49.0'
+    browserName: 'firefox',
+    platform: 'Windows 10',
+    version: '49.0'
+  }]
 })
