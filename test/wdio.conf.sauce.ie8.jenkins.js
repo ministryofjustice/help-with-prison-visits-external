c3c87f59435fde53e15a9c4572ce97252f1585eb
Gary Douglas <garyd@kainos.com>
2017-01-31 15:28:06 +0000

    Added wdio-conf-helper to merge configs

--- test/wdio.conf.sauce.ie8.jenkins.js
+++ test/wdio.conf.sauce.ie8.jenkins.js
@@ -1,12 +1,12 @@
-const merge = require('deepmerge')
-const wdioConf = require('./wdio.conf')
+const wdioConfHelper = require('./helpers/wdio-conf-helper')
 
-exports.config = merge(wdioConf.config, {
+exports.config = wdioConfHelper({
   services: ['sauce'],
   user: process.env.SAUCE_USERNAME,
   key: process.env.SAUCE_ACCESS_KEY,
   baseUrl: process.env.SAUCE_BASEURL || 'http://localhost:3000',
   capabilities: [{
+    maxInstances: 1,
     browserName: 'internet explorer',
     platform: 'Windows XP',
     version: '8.0'
