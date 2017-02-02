b884976d5ee88486e83b3f12688139bc99a07ef3
Gary Douglas <garyd@kainos.com>
2017-02-02 10:48:37 +0000

    Added test for mask-string

--- app/services/helpers/mask-string.js
+++ app/services/helpers/mask-string.js
@@ -1,4 +1,4 @@
 module.exports = function (unmaskedValue, visibleLetterCount) {
-  var result = `${unmaskedValue.substring(0, visibleLetterCount)}${'*'.repeat(unmaskedValue.length - 1)}`
+  var result = `${unmaskedValue.substring(0, visibleLetterCount)}${'*'.repeat(unmaskedValue.length - visibleLetterCount)}`
   return result
 }
