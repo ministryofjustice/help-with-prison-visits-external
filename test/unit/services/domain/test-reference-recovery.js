57220262f463c5d22848fb59da641a76d0f97149
Michael Hall <m.hall@kainos.com>
2017-02-06 14:14:02 +0000

    Domain test

---
+++ test/unit/services/domain/test-reference-recovery.js
@@ -0,0 +1,28 @@
+const ReferenceRecovery = require('../../../../app/services/domain/reference-recovery')
+const ValidationError = require('../../../../app/services/errors/validation-error')
+const expect = require('chai').expect
+
+describe('services/domain/reference-recovery', function () {
+  const VALID_EMAIL = 'test@test.com'
+  const VALID_PRISONER_NUMBER = 'B2781937'
+  const INVALID_EMAIL = 'test'
+  const INVALID_PRISONER_NUMBER = ''
+
+  it('should construct a domain object given valid input', function () {
+    var referenceRecovery = new ReferenceRecovery(VALID_EMAIL, VALID_PRISONER_NUMBER)
+    expect(referenceRecovery.EmailAddress).to.equal(VALID_EMAIL)
+    expect(referenceRecovery.PrisonerNumber).to.equal(VALID_PRISONER_NUMBER)
+  })
+
+  it('should throw an error if passed invalid email address', function () {
+    expect(function () {
+      new ReferenceRecovery(INVALID_EMAIL, VALID_PRISONER_NUMBER).isValid()
+    }).to.throw(ValidationError)
+  })
+
+  it('should throw an error if passed invalid prisoner number', function () {
+    expect(function () {
+      new ReferenceRecovery(VALID_EMAIL, INVALID_PRISONER_NUMBER).isValid()
+    }).to.throw(ValidationError)
+  })
+})
