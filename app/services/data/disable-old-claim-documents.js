dc215e583694c8610603628691a64cbd44926c02
Michael Hall <m.hall@kainos.com>
2017-02-09 16:00:07 +0000

    Added integration to disable old claim docs if not multipage

---
+++ app/services/data/disable-old-claim-documents.js
@@ -0,0 +1,23 @@
+const config = require('../../../knexfile').extweb
+const knex = require('knex')(config)
+const FileUpload = require('../domain/file-upload')
+const Promise = require('bluebird')
+
+module.exports = function (reference, claimId, claimExpenseId, fileUpload, multipageDoc) {
+  if (!(fileUpload instanceof FileUpload)) {
+    throw new Error('Provided fileUpload object is not an instance of the expected class')
+  }
+
+  if (multipageDoc) {
+    return Promise.resolve()
+  } else {
+    return knex('ClaimDocument')
+      .update('IsEnabled', false)
+      .where({
+        'Reference': reference,
+        'ClaimId': claimId,
+        'ClaimExpenseId': claimExpenseId,
+        'DocumentType': fileUpload.documentType
+      })
+  }
+}
