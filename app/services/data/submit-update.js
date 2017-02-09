9337a968de5b5050dcc1c34390703d01d17afdab
Steven Alexander <steven.william.alexander@googlemail.com>
2017-02-09 16:20:37 +0000

    moved insert REQUEST-INFORMATION-RESPONSE-SUBMITTED-NOTIFICATION so it will occur after REQUEST-INFORMATION-RESPONSE in async worker

--- app/services/data/submit-update.js
+++ app/services/data/submit-update.js
@@ -1,31 +1,22 @@
 const Promise = require('bluebird')
 const insertTask = require('./insert-task')
 const tasksEnum = require('../../constants/tasks-enum')
-const getRepeatEligibility = require('./get-repeat-eligibility')
 const insertBankDetails = require('./insert-bank-account-details-for-claim')
 const BankAccountDetails = require('../domain/bank-account-details')
 
 module.exports = function (reference, eligibilityId, claimId, message, bankDetails) {
-  var tasks = [
-    insertTask(reference, eligibilityId, claimId, tasksEnum.REQUEST_INFORMATION_RESPONSE, message),
-    insertTaskRequestInformationResponseSubmittedNotification(reference, eligibilityId, claimId)
-  ]
+  return insertBankDetailsIfRequired(reference, eligibilityId, claimId, bankDetails)
+    .then(function () {
+      return insertTask(reference, eligibilityId, claimId, tasksEnum.REQUEST_INFORMATION_RESPONSE, message)
+    })
+}
 
+function insertBankDetailsIfRequired (reference, eligibilityId, claimId, bankDetails) {
   if (bankDetails.required) {
     var termsAcceptedForUpdate = 'yes'
     var bankAccountDetails = new BankAccountDetails(bankDetails.accountNumber, bankDetails.sortCode, termsAcceptedForUpdate)
-    tasks.push(insertBankDetails(reference, eligibilityId, claimId, bankAccountDetails))
+    return insertBankDetails(reference, eligibilityId, claimId, bankAccountDetails)
+  } else {
+    return Promise.resolve()
   }
-
-  return Promise.all(tasks)
-}
-
-function insertTaskRequestInformationResponseSubmittedNotification (reference, eligibilityId, claimId) {
-  return getRepeatEligibility(reference, null, eligibilityId)
-    .then(function (result) {
-      if (!result) {
-        throw new Error('Could not find email address to send notification')
-      }
-      return insertTask(reference, eligibilityId, claimId, tasksEnum.REQUEST_INFORMATION_RESPONSE_SUBMITTED_NOTIFICATION, result.EmailAddress)
-    })
 }
