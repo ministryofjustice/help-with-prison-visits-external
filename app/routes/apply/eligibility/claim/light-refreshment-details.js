0375d6ce9938de138f2ced77418410caae6bb770
Michael Hall <m.hall@kainos.com>
2017-03-24 14:14:47 +0000

    Made light refreshment and accommodation say estimated cost on advance (#524)

--- app/routes/apply/eligibility/claim/light-refreshment-details.js
+++ app/routes/apply/eligibility/claim/light-refreshment-details.js
@@ -4,18 +4,23 @@ const ValidationError = require('../../../../services/errors/validation-error')
 const expenseUrlRouter = require('../../../../services/routing/expenses-url-router')
 const RefreshmentExpense = require('../../../../services/domain/expenses/refreshment-expense')
 const insertExpense = require('../../../../services/data/insert-expense')
+const getIsAdvanceClaim = require('../../../../services/data/get-is-advance-claim')
 
 module.exports = function (router) {
   router.get('/apply/:claimType/eligibility/:referenceId/claim/:claimId/refreshment', function (req, res) {
     UrlPathValidator(req.params)
 
-    return res.render('apply/eligibility/claim/light-refreshment-details', {
-      claimType: req.params.claimType,
-      referenceId: req.params.referenceId,
-      claimId: req.params.claimId,
-      params: expenseUrlRouter.parseParams(req.query),
-      redirectUrl: expenseUrlRouter.getRedirectUrl(req)
-    })
+    getIsAdvanceClaim(req.params.claimId)
+      .then(function (isAdvanceClaim) {
+        return res.render('apply/eligibility/claim/light-refreshment-details', {
+          claimType: req.params.claimType,
+          referenceId: req.params.referenceId,
+          claimId: req.params.claimId,
+          params: expenseUrlRouter.parseParams(req.query),
+          redirectUrl: expenseUrlRouter.getRedirectUrl(req),
+          isAdvanceClaim: isAdvanceClaim
+        })
+      })
   })
 
   router.post('/apply/:claimType/eligibility/:referenceId/claim/:claimId/refreshment', function (req, res, next) {
@@ -34,15 +39,19 @@ module.exports = function (router) {
         })
     } catch (error) {
       if (error instanceof ValidationError) {
-        return res.status(400).render('apply/eligibility/claim/light-refreshment-details', {
-          errors: error.validationErrors,
-          claimType: req.params.claimType,
-          referenceId: req.params.referenceId,
-          claimId: req.params.claimId,
-          params: expenseUrlRouter.parseParams(req.query),
-          redirectUrl: expenseUrlRouter.getRedirectUrl(req),
-          expense: req.body
-        })
+        getIsAdvanceClaim(req.params.claimId)
+          .then(function (isAdvanceClaim) {
+            return res.status(400).render('apply/eligibility/claim/light-refreshment-details', {
+              errors: error.validationErrors,
+              claimType: req.params.claimType,
+              referenceId: req.params.referenceId,
+              claimId: req.params.claimId,
+              params: expenseUrlRouter.parseParams(req.query),
+              redirectUrl: expenseUrlRouter.getRedirectUrl(req),
+              expense: req.body,
+              isAdvanceClaim: isAdvanceClaim
+            })
+          })
       } else {
         throw error
       }
