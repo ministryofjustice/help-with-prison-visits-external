c5ac2fd05feda3ab438deeb9ab92a1a35ceacbc5
Michael Hall <m.hall@kainos.com>
2017-04-03 10:32:05 +0100

    Add logic to handle double submission on the payment details page (#532)
    Added DB check of claim current status
    Added logic in route to assume claim already submitted to prevent double submission attempt

---
+++ test/integration/services/data/test-check-status-for-finishing-claim.js
@@ -0,0 +1,40 @@
+const expect = require('chai').expect
+const checkStatusForFinishingClaim = require('../../../../app/services/data/check-status-for-finishing-claim')
+const eligibilityHelper = require('../../../helpers/data/eligibility-helper')
+const config = require('../../../../knexfile').extweb
+const knex = require('knex')(config)
+
+describe('services/data/check-status-for-finishing-claim', function () {
+  const REFERENCE = 'FINSTAT'
+  var claimId
+  var eligibilityId
+
+  beforeEach(function () {
+    return eligibilityHelper.insertEligibilityClaim(REFERENCE)
+      .then(function (ids) {
+        claimId = ids.claimId
+        eligibilityId = ids.eligibilityId
+      })
+  })
+
+  afterEach(function () {
+    return eligibilityHelper.deleteAll(REFERENCE)
+  })
+
+  it('should return true for claim that is in progress', function () {
+    return checkStatusForFinishingClaim(REFERENCE, eligibilityId, claimId)
+      .then(function (result) {
+        expect(result).to.be.true
+      })
+  })
+
+  it('should return false claim not being in progress', function () {
+    return knex('Claim').update({'Status': 'NOT-IN-PROGRESS'}).where({'ClaimId': claimId, 'Reference': REFERENCE})
+      .then(function () {
+        return checkStatusForFinishingClaim(REFERENCE, eligibilityId, claimId)
+          .then(function (result) {
+            expect(result).to.be.false
+          })
+      })
+  })
+})
