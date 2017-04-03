04d77de44ca3fcb6c2e1fc92076385cff10d1354
Michael Hall <m.hall@kainos.com>
2017-04-03 11:33:16 +0100

    Changed check from time based to date based (#534)

--- test/integration/services/data/test-insert-new-claim.js
+++ test/integration/services/data/test-insert-new-claim.js
@@ -2,6 +2,7 @@ const expect = require('chai').expect
 const insertNewClaim = require('../../../../app/services/data/insert-new-claim')
 const eligiblityHelper = require('../../../helpers/data/eligibility-helper')
 const claimHelper = require('../../../helpers/data/claim-helper')
+const moment = require('moment')
 
 describe('services/data/insert-new-claim', function () {
   const REFERENCE = 'APVS137'
@@ -27,10 +28,7 @@ describe('services/data/insert-new-claim', function () {
         expect(claim.Reference).to.be.equal(REFERENCE)
         expect(claim.ClaimType).to.be.equal(CLAIM_TYPE)
         expect(claim.IsAdvanceClaim).to.be.equal(claimHelper.IS_ADVANCE_CLAIM)
-        expect(claim.DateOfJourney).to.be.within(
-          claimHelper.DATE_OF_JOURNEY.subtract(1, 'seconds').toDate(),
-          claimHelper.DATE_OF_JOURNEY.add(1, 'seconds').toDate()
-        )
+        expect(moment(claim.DateOfJourney).format('DD/MM/YYYY')).to.equal(claimHelper.DATE_OF_JOURNEY.format('DD/MM/YYYY'))
         expect(claim.DateSubmitted).to.be.equal(null)
         expect(claim.Status).to.equal(claimHelper.STATUS)
       })
