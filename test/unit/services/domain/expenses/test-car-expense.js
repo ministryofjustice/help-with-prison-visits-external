da26305bb7538a9220e496ca7662c4c6bfef4c79
Michael Hall <m.hall@kainos.com>
2017-04-06 15:51:02 +0100

    Updated domain tests

--- test/unit/services/domain/expenses/test-car-expense.js
+++ test/unit/services/domain/expenses/test-car-expense.js
@@ -9,8 +9,12 @@ describe('services/domain/expenses/car-expense', function () {
   const VALID_TOLL_COST = '15'
   const VALID_PARKING = 'yes'
   const VALID_PARKING_COST = '20'
+  const VALID_NEW_DESTINATION = 'on'
+  const VALID_DESTINATION = 'some place'
+  const VALID_POSTCODE = 'BT123BT'
   const INVALID_TOLL_COST = '0'
   const INVALID_PARKING_COST = '0'
+  const INVALID_POSTCODE = 'testing'
 
   it('should construct a domain object given valid input', function () {
     var expense = new CarExpense(
@@ -31,6 +35,29 @@ describe('services/domain/expenses/car-expense', function () {
     expect(expense.parkingExpense).to.not.equal(null)
   })
 
+  it('should construct a domain object given valid input of new destination and postcode', function () {
+    var expense = new CarExpense(
+      VALID_FROM,
+      VALID_TO,
+      VALID_TOLL,
+      VALID_TOLL_COST,
+      VALID_PARKING,
+      VALID_PARKING_COST,
+      VALID_NEW_DESTINATION,
+      VALID_DESTINATION,
+      VALID_POSTCODE
+    )
+    expect(expense.from).to.equal(VALID_FROM)
+    expect(expense.to).to.equal(VALID_DESTINATION)
+    expect(expense.toll).to.equal(VALID_TOLL)
+    expect(expense.tollCost).to.equal(VALID_TOLL_COST)
+    expect(expense.parking).to.equal(VALID_PARKING)
+    expect(expense.parkingCost).to.equal(VALID_PARKING_COST)
+    expect(expense.toPostCode).to.equal(VALID_POSTCODE)
+    expect(expense.tollExpense).to.not.equal(null)
+    expect(expense.parkingExpense).to.not.equal(null)
+  })
+
   it('should not return null for toll cost if toll was not set', function () {
     var expense = new CarExpense(
       VALID_FROM,
@@ -45,6 +72,38 @@ describe('services/domain/expenses/car-expense', function () {
       VALID_TO
     )
     expect(expense.parkingExpense).to.equal(null)
+  })
+
+  it('should throw an error if passed invalid destination', function () {
+    expect(function () {
+      new CarExpense(
+        VALID_FROM,
+        VALID_TO,
+        VALID_TOLL,
+        VALID_TOLL_COST,
+        VALID_PARKING,
+        VALID_PARKING_COST,
+        VALID_NEW_DESTINATION,
+        '',
+        VALID_POSTCODE
+      ).isValid()
+    }).to.throw(ValidationError)
+  })
+
+  it('should throw an error if passed invalid postcode', function () {
+    expect(function () {
+      new CarExpense(
+        VALID_FROM,
+        VALID_TO,
+        VALID_TOLL,
+        VALID_TOLL_COST,
+        VALID_PARKING,
+        VALID_PARKING_COST,
+        VALID_NEW_DESTINATION,
+        VALID_DESTINATION,
+        INVALID_POSTCODE
+      ).isValid()
+    }).to.throw(ValidationError)
   })
 
   it('should throw an error if passed invalid toll cost', function () {
