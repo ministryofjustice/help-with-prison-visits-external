ad5be625b2e1f696245aaafb0ef9219a5c5f932f
Steven Alexander <steven.william.alexander@googlemail.com>
2017-05-18 12:14:03 +0100

    added data protection guidance to file upload

--- app/constants/document-type-enum.js
+++ app/constants/document-type-enum.js
@@ -1,10 +1,10 @@
 module.exports = {
-  'VISIT_CONFIRMATION': {documentType: 'VISIT-CONFIRMATION', title: 'Add visit confirmation form', guidingText: 'Provide the stamped form as evidence of your visit.'},
-  'working-tax-credit': {documentType: 'working-tax-credit', title: 'Working Tax credits or Child tax credit with disability documentation', guidingText: 'You must provide evidence of each page of your Working Tax credits or Child tax credit with disability documentation, upload one page at a time'},
-  'pension-credit': {documentType: 'pension-credit', title: 'Pension credit documentation', guidingText: 'You must provide evidence of each page of your Pension Credit documentation, upload one page at a time'},
-  'hc2': {documentType: 'hc2', title: 'Health Certificate (HC2) documentation', guidingText: 'You must provide evidence of your Health Certificate (HC2) documentation'},
-  'hc3': {documentType: 'hc3', title: 'Health Certificate (HC3) documentation', guidingText: 'You must provide evidence of your Health Certificate (HC3) documentation'},
-  'nhs-tax-credit': {documentType: 'nhs-tax-credit', title: 'NHS Tax credit documentation', guidingText: 'You must provide evidence of your NHS Tax credit documentation'},
-  'universal-credit': {documentType: 'universal-credit', title: 'Universal credit documentation', guidingText: 'You must provide evidence of your Universal credit documentation'},
-  'RECEIPT': {documentType: 'RECEIPT', title: 'Add receipt', guidingText: 'Provide a receipt or a copy of your tickets.'}
+  'VISIT_CONFIRMATION': {documentType: 'VISIT-CONFIRMATION', title: 'Add visit confirmation form', guidingText: 'Provide the stamped form as evidence of your visit.', displayDataProtection: false},
+  'working-tax-credit': {documentType: 'working-tax-credit', title: 'Working Tax credits or Child tax credit with disability documentation', guidingText: 'You must provide evidence of each page of your Working Tax credits or Child tax credit with disability documentation, upload one page at a time', displayDataProtection: true},
+  'pension-credit': {documentType: 'pension-credit', title: 'Pension credit documentation', guidingText: 'You must provide evidence of each page of your Pension Credit documentation, upload one page at a time', displayDataProtection: true},
+  'hc2': {documentType: 'hc2', title: 'Health Certificate (HC2) documentation', guidingText: 'You must provide evidence of your Health Certificate (HC2) documentation', displayDataProtection: true},
+  'hc3': {documentType: 'hc3', title: 'Health Certificate (HC3) documentation', guidingText: 'You must provide evidence of your Health Certificate (HC3) documentation', displayDataProtection: true},
+  'nhs-tax-credit': {documentType: 'nhs-tax-credit', title: 'NHS Tax credit documentation', guidingText: 'You must provide evidence of your NHS Tax credit documentation', displayDataProtection: true},
+  'universal-credit': {documentType: 'universal-credit', title: 'Universal credit documentation', guidingText: 'You must provide evidence of your Universal credit documentation', displayDataProtection: true},
+  'RECEIPT': {documentType: 'RECEIPT', title: 'Add receipt', guidingText: 'Provide a receipt or a copy of your tickets.', displayDataProtection: false}
 }
