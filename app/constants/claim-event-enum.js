64691ca7d4ceb03cea04620b0999c4e99be382a0
tsheppard01 <t.sheppard@kainos.com>
2017-07-26 13:35:31 +0100

    APVS-0036: New enum to show correct text for close-advance-claims event.

--- app/constants/claim-event-enum.js
+++ app/constants/claim-event-enum.js
@@ -1,5 +1,6 @@
 module.exports = {
   'CLAIM-APPROVED': 'Claim approved',
   'CLAIM-REJECTED': 'Claim rejected',
-  'CLAIM-REQUEST-INFORMATION': 'Requested Information'
+  'CLAIM-REQUEST-INFORMATION': 'Requested Information',
+  'CLOSE-ADVANCE-CLAIM': 'Advance claim closed'
 }
