52ca9ebfc59939029edcafccc7b1baf3f3ef1618
kevinfox <kevinfox@kainos.com>
2017-11-15 16:08:37 +0000

    Fixed coding standards

--- app/services/data/get-claim-expense-by-id-or-last-approved.js
+++ app/services/data/get-claim-expense-by-id-or-last-approved.js
@@ -3,9 +3,9 @@ const knex = require('knex')(config)
 
 module.exports = function (reference, eligibiltyId, claimId) {
   return knex.raw(`SELECT * FROM [IntSchema].[getLastClaimForReference] (?, ?)`, [ reference, eligibiltyId ])
-  .then(function (claimIdReturned){
+  .then(function (claimIdReturned) {
     console.log(claimIdReturned)
-    if(claimIdReturned.length > 0) {
+    if (claimIdReturned.length > 0) {
       claimId = parseInt(claimIdReturned[0].ClaimId)
     }
     return knex.raw(`SELECT * FROM [IntSchema].[getClaimExpenseByIdOrLastApproved] (?, ?, ?)`, [ reference, eligibiltyId, claimId ])
