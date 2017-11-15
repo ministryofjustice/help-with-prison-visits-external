757c040339ae89ce25704a90785cfb498eb5be44
kevinfox <kevinfox@kainos.com>
2017-11-15 15:59:53 +0000

    Fixed issue that would not allow a claimant to view a specifc claim

--- app/services/data/get-claim-expense-by-id-or-last-approved.js
+++ app/services/data/get-claim-expense-by-id-or-last-approved.js
@@ -4,7 +4,11 @@ const knex = require('knex')(config)
 module.exports = function (reference, eligibiltyId, claimId) {
   return knex.raw(`SELECT * FROM [IntSchema].[getLastClaimForReference] (?, ?)`, [ reference, eligibiltyId ])
   .then(function (claimIdReturned){
-    return knex.raw(`SELECT * FROM [IntSchema].[getClaimExpenseByIdOrLastApproved] (?, ?, ?)`, [ reference, eligibiltyId, parseInt(claimIdReturned[0].ClaimId) ])
+    console.log(claimIdReturned)
+    if(claimIdReturned.length > 0) {
+      claimId = parseInt(claimIdReturned[0].ClaimId)
+    }
+    return knex.raw(`SELECT * FROM [IntSchema].[getClaimExpenseByIdOrLastApproved] (?, ?, ?)`, [ reference, eligibiltyId, claimId ])
     .then(function (claimExpenses) {
       claimExpenses.forEach(function (expense) {
         if (!expense.Cost) {
