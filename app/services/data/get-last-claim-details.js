90adeed9ae4b047445d6185c7c7d2f33673a8df9
kevinfox <kevinfox@kainos.com>
2017-11-16 13:47:35 +0000

    Fixed indentation errors

--- app/services/data/get-last-claim-details.js
+++ app/services/data/get-last-claim-details.js
@@ -8,32 +8,31 @@ module.exports = function (reference, eligibilityId, mask) {
   var result = {}
   var claimId = null
   return getLastClaimForReference(reference, eligibilityId)
-  .then(function (claimIdReturned) {
-    console.log(claimIdReturned)
-    if (claimIdReturned.length > 0) {
-      claimId = parseInt(claimIdReturned[0].ClaimId)
-    }
-    return getClaimChildrenByIdOrLastApproved(reference, eligibilityId, claimId)
-      .then(function (lastClaimChildren) {
-        if (mask) {
-          result.children = maskArrayOfNames(lastClaimChildren)
-        } else {
-          result.children = lastClaimChildren
-        }
-
-        return getClaimExpenseByIdOrLastApproved(reference, eligibilityId, claimId)
-      })
-      .then(function (lastClaimExpenses) {
-        result.expenses = lastClaimExpenses
-        return getClaimEscortByIdOrLastApproved(reference, eligibilityId, claimId)
-      })
-      .then(function (lastClaimEscort) {
-        if (mask) {
-          result.escort = maskArrayOfNames(lastClaimEscort)
-        } else {
-          result.escort = lastClaimEscort
-        }
-        return result
-      })
+    .then(function (claimIdReturned) {
+      console.log(claimIdReturned)
+      if (claimIdReturned.length > 0) {
+        claimId = parseInt(claimIdReturned[0].ClaimId)
+      }
+      return getClaimChildrenByIdOrLastApproved(reference, eligibilityId, claimId)
+        .then(function (lastClaimChildren) {
+          if (mask) {
+            result.children = maskArrayOfNames(lastClaimChildren)
+          } else {
+            result.children = lastClaimChildren
+          }
+          return getClaimExpenseByIdOrLastApproved(reference, eligibilityId, claimId)
+        })
+        .then(function (lastClaimExpenses) {
+          result.expenses = lastClaimExpenses
+          return getClaimEscortByIdOrLastApproved(reference, eligibilityId, claimId)
+        })
+        .then(function (lastClaimEscort) {
+          if (mask) {
+            result.escort = maskArrayOfNames(lastClaimEscort)
+          } else {
+            result.escort = lastClaimEscort
+          }
+          return result
+        })
     })
 }
