01f16b92063adca62f266a2ee209a08a191d093b
kevinfox <kevinfox@kainos.com>
2017-11-16 13:35:56 +0000

    Refactored stored function getLastClaimForReference call out into its own file so that it may be called for getting claim children and claim escorts as well as claim expenses

--- app/services/data/get-last-claim-details.js
+++ app/services/data/get-last-claim-details.js
@@ -1,32 +1,39 @@
 const getClaimChildrenByIdOrLastApproved = require('./get-claim-children-by-id-or-last-approved')
 const getClaimExpenseByIdOrLastApproved = require('./get-claim-expense-by-id-or-last-approved')
 const getClaimEscortByIdOrLastApproved = require('./get-claim-escort-by-id-or-last-approved')
+const getLastClaimForReference = require('./get-last-claim-for-reference')
 const maskArrayOfNames = require('../helpers/mask-array-of-names')
 
 module.exports = function (reference, eligibilityId, mask) {
   var result = {}
+  var claimId = null
+  return getLastClaimForReference(reference, eligibilityId)
+  .then(function (claimIdReturned) {
+    console.log(claimIdReturned)
+    if (claimIdReturned.length > 0) {
+      claimId = parseInt(claimIdReturned[0].ClaimId)
+    }
+    return getClaimChildrenByIdOrLastApproved(reference, eligibilityId, claimId)
+      .then(function (lastClaimChildren) {
+        if (mask) {
+          result.children = maskArrayOfNames(lastClaimChildren)
+        } else {
+          result.children = lastClaimChildren
+        }
 
-  return getClaimChildrenByIdOrLastApproved(reference, eligibilityId, null)
-    .then(function (lastClaimChildren) {
-      if (mask) {
-        result.children = maskArrayOfNames(lastClaimChildren)
-      } else {
-        result.children = lastClaimChildren
-      }
-
-      return getClaimExpenseByIdOrLastApproved(reference, eligibilityId, null)
-    })
-    .then(function (lastClaimExpenses) {
-      result.expenses = lastClaimExpenses
-      return getClaimEscortByIdOrLastApproved(reference, eligibilityId, null)
-    })
-    .then(function (lastClaimEscort) {
-      if (mask) {
-        result.escort = maskArrayOfNames(lastClaimEscort)
-      } else {
-        result.escort = lastClaimEscort
-      }
-
-      return result
+        return getClaimExpenseByIdOrLastApproved(reference, eligibilityId, claimId)
+      })
+      .then(function (lastClaimExpenses) {
+        result.expenses = lastClaimExpenses
+        return getClaimEscortByIdOrLastApproved(reference, eligibilityId, claimId)
+      })
+      .then(function (lastClaimEscort) {
+        if (mask) {
+          result.escort = maskArrayOfNames(lastClaimEscort)
+        } else {
+          result.escort = lastClaimEscort
+        }
+        return result
+      })
     })
 }
