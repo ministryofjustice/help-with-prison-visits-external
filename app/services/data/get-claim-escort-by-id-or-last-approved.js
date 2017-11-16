036c007c93db97c4d33dbfaf372e1cad03fd47f2
kevinfox <kevinfox@kainos.com>
2017-11-16 11:46:59 +0000

    Calling new getLastClaimForReference before getting children and escorts as similar issues are occuring with these

--- app/services/data/get-claim-escort-by-id-or-last-approved.js
+++ app/services/data/get-claim-escort-by-id-or-last-approved.js
@@ -2,5 +2,12 @@ const config = require('../../../knexfile').extweb
 const knex = require('knex')(config)
 
 module.exports = function (reference, eligibiltyId, claimId) {
-  return knex.raw(`SELECT * FROM [IntSchema].[getClaimEscortByIdOrLastApproved] (?, ?, ?)`, [ reference, eligibiltyId, claimId ])
+  return knex.raw(`SELECT * FROM [IntSchema].[getLastClaimForReference] (?, ?)`, [ reference, eligibiltyId ])
+  .then(function (claimIdReturned) {
+    console.log(claimIdReturned)
+    if (claimIdReturned.length > 0) {
+      claimId = parseInt(claimIdReturned[0].ClaimId)
+    }
+    return knex.raw(`SELECT * FROM [IntSchema].[getClaimEscortByIdOrLastApproved] (?, ?, ?)`, [ reference, eligibiltyId, claimId ])
+  })
 }
