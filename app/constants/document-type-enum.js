b7ffc2598a1335ef6f0f200bbc020af7e9872ee3
kevinfox <kevinfox@kainos.com>
2017-11-30 11:43:04 +0000

    Removing APVS0062 Changes to enable separate relase of this branch

--- app/constants/document-type-enum.js
+++ app/constants/document-type-enum.js
@@ -6,8 +6,5 @@ module.exports = {
   'hc3': {documentType: 'hc3', title: 'Health Certificate (HC3) documentation', guidingText: 'You must provide evidence of your Health Certificate (HC3) documentation', displayDataProtection: true},
   'nhs-tax-credit': {documentType: 'nhs-tax-credit', title: 'NHS Tax credit documentation', guidingText: 'You must provide evidence of your NHS Tax credit documentation', displayDataProtection: true},
   'universal-credit': {documentType: 'universal-credit', title: 'Universal credit documentation', guidingText: 'You must provide evidence of your Universal credit documentation', displayDataProtection: true},
-  'RECEIPT': {documentType: 'RECEIPT', title: 'Add receipt', guidingText: 'Provide a receipt or a copy of your tickets.', displayDataProtection: false},
-  'income-support': {documentType: 'income-support', title: 'Income Support', guidingText: 'You must provide evidence of each page of your Income Support documentation, upload one page at a time', displayDataProtection: true},
-  'jobseekers-allowance': {documentType: 'jobseekers-allowance', title: 'Jobseeker\'s Allowance', guidingText: 'You must provide evidence of each page of your Jobseeker\'s Allowance documentation, upload one page at a time', displayDataProtection: true},
-  'employment-support': {documentType: 'employment-support', title: 'Employment and Support Allowance', guidingText: 'You must provide evidence of each page of your Employment and Support Allowance documentation, upload one page at a time', displayDataProtection: true}
+  'RECEIPT': {documentType: 'RECEIPT', title: 'Add receipt', guidingText: 'Provide a receipt or a copy of your tickets.', displayDataProtection: false}
 }
