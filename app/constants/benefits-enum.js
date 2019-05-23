587b52df3fefcb97a90f6e9ecf2ee0784d3f5018
kevinfox <kevinfox@kainos.com>
2019-05-23 16:19:21 +0100

    APVS0201: Enabling multipage document uploads

--- app/constants/benefits-enum.js
+++ app/constants/benefits-enum.js
@@ -29,7 +29,7 @@ module.exports = {
     value: 'universal-credit',
     requireBenefitUpload: true,
     displayName: 'Universal Credit',
-    multipage: false,
+    multipage: true,
     urlValue: 'b4'
   },
 
