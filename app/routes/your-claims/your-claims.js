ed97d8d4cb28b64e1c75e1c5d3b2809755001cae
kevinfox <kevinfox@kainos.com>
2020-07-08 10:04:23 +0100

    APVS0230: Fixing an issue with the way the date of birth was being formatted

--- app/routes/your-claims/your-claims.js
+++ app/routes/your-claims/your-claims.js
@@ -22,7 +22,7 @@ module.exports = function (router) {
 
     var decodedDOB = dateFormatter.decodeDate(req.session.dobEncoded)
 
-    getHistoricClaims(req.session.decryptedRef, dateFormatter.buildFromDateString(decodedDOB).toDate())
+    getHistoricClaims(req.session.decryptedRef, decodedDOB)
       .then(function (claims) {
         if (claims.length === 0) {
           return res.redirect(`/start-already-registered${REFERENCE_DOB_INCORRECT_ERROR}`)
