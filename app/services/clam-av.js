7f7eb99c5eb4e7b57a0f46f06700ff0a9e35b3a5
kevinfox <kevinfox@kainos.com>
2020-10-21 12:09:18 +0100

    Updating ClamAV to the latest version

--- app/services/clam-av.js
+++ app/services/clam-av.js
@@ -1,9 +1,10 @@
 const config = require('../../config')
 var Promise = require('bluebird').Promise
+const NodeClam = require('clamscan')
 
 var clam
 try {
-  clam = require('clamscan')({
+  clam = new NodeClam().init({
     remove_infected: config.CLAM_REMOVE_INFECTED,
     quarantine_infected: false,
     clamdscan: {
@@ -20,14 +21,14 @@ try {
 }
 
 module.exports.scan = function (filePath) {
-  return new Promise(function (resolve, reject) {
-    if (config.ENABLE_MALWARE_SCANNING === 'true') {
-      clam.is_infected(filePath, function (error, file, infected) {
-        if (error) reject(error)
-        return resolve(infected)
+  if (config.ENABLE_MALWARE_SCANNING === 'true') {
+    clam.then(clamscan => {
+      clamscan.is_infected(filePath, (error, file, isInfected, viruses) => {
+        if (error) throw error
+        return isInfected
       })
-    } else {
-      return resolve(false)
-    }
-  })
+    })
+  } else {
+    return Promise.resolve(false)
+  }
 }
