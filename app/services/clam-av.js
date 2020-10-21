5087d06dca5f7fc92d912dc96d4ae8a227258fc2
kevinfox <kevinfox@kainos.com>
2020-10-21 12:20:21 +0100

    Updating ClamAV to the latest version

--- app/services/clam-av.js
+++ app/services/clam-av.js
@@ -25,7 +25,7 @@ module.exports.scan = function (filePath) {
     clam.then(clamscan => {
       clamscan.is_infected(filePath, (error, file, isInfected, viruses) => {
         if (error) throw error
-        return isInfected
+        return Promise.resolve(isInfected)
       })
     })
   } else {
