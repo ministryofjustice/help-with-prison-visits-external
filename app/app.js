cd65458de9c1d0de76448167a21a359004142774
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2021-06-11 14:18:08 +0100

    Remove Gulp

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 require('./azure-appinsights')
 const config = require('../config')
 const express = require('express')
-const nunjucks = require('express-nunjucks')
+const nunjucks = require('nunjucks')
 const path = require('path')
 const favicon = require('serve-favicon')
 const bodyParser = require('body-parser')
@@ -63,18 +63,27 @@ const developmentMode = app.get('env') === 'development'
 const releaseVersion = packageJson.version
 const serviceName = 'Get help with the cost of prison visits'
 
-app.set('view engine', 'html')
-app.set('views', path.join(__dirname, 'views'))
+const appViews = [
+  path.join(__dirname, '../node_modules/govuk_template_jinja/'),
+  path.join(__dirname, 'views')
+]
 
-nunjucks(app, {
+// View Engine Configuration
+app.set('view engine', 'html')
+nunjucks.configure(appViews, {
+  express: app,
+  autoescape: true,
   watch: developmentMode,
   noCache: developmentMode
 })
 
-app.use('/public', express.static(path.join(__dirname, 'public')))
-app.use('/public', express.static(path.join(__dirname, 'govuk_modules', 'govuk_template')))
-app.use('/public', express.static(path.join(__dirname, 'govuk_modules', 'govuk_frontend_toolkit')))
-app.use(favicon(path.join(__dirname, 'govuk_modules', 'govuk_template', 'images', 'favicon.ico')))
+const publicFolders = ['public', 'assets', '../node_modules/govuk_template_jinja/assets', '../node_modules/govuk_frontend_toolkit']
+
+publicFolders.forEach(dir => {
+  app.use('/public', express.static(path.join(__dirname, dir)))
+})
+
+app.use(favicon(path.join(__dirname, '../node_modules/govuk_template_jinja/assets/images/favicon.ico')))
 
 // Basic auth
 if (config.BASIC_AUTH_ENABLED === 'true') {
--- gulpfile.js
+++ gulpfile.js
@@ -2,32 +2,6 @@ const gulp = require('gulp')
 const sass = require('gulp-sass')
 const spawn = require('child_process').spawn
 
-gulp.task('assets', function (done) {
-  gulp.src('node_modules/govuk_frontend_toolkit/javascripts/**/*')
-    .pipe(gulp.dest('app/govuk_modules/govuk_frontend_toolkit/javascripts', { overwrite: true }))
-
-  gulp.src('node_modules/govuk_frontend_toolkit/images/**/*')
-    .pipe(gulp.dest('app/govuk_modules/govuk_frontend_toolkit/images', { overwrite: true }))
-
-  gulp.src('node_modules/govuk_template_jinja/assets/**/*')
-    .pipe(gulp.dest('app/govuk_modules/govuk_template/', { overwrite: true }))
-  done()
-})
-
-gulp.task('templates', function (done) {
-  gulp.src('node_modules/govuk_template_jinja/views/layouts/govuk_template.html')
-    .pipe(gulp.dest('app/views/', { overwrite: true }))
-  done()
-})
-
-gulp.task('sync', function (done) {
-  gulp.src('app/assets/javascripts/**/*')
-    .pipe(gulp.dest('app/public/javascripts/', { overwrite: true }))
-  gulp.src('app/assets/images/**/*')
-    .pipe(gulp.dest('app/public/images/', { overwrite: true }))
-  done()
-})
-
 gulp.task('sass', function (done) {
   gulp.src('app/assets/sass/**/*.scss')
     .pipe(sass({
@@ -46,9 +20,7 @@ gulp.task('sass', function (done) {
   done()
 })
 
-gulp.task('generate-assets', gulp.series('assets', 'templates', 'sync', 'sass'))
-
-gulp.task('generate-assets-and-start', gulp.series('generate-assets', function () {
+gulp.task('generate-assets-and-start', gulp.series('sass', function () {
   spawn('node', ['app/bin/www'], { stdio: 'inherit' })
 }))
 
