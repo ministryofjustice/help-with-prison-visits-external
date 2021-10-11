359d028c284afd2ccd82a670c3bc438a14a46673
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2021-10-11 16:46:01 +0100

    Update '/' redirect test
    Don't actually follow redirect - just test response.

--- cypress/integration/govuk_redirect_spec.js
+++ cypress/integration/govuk_redirect_spec.js
@@ -2,8 +2,13 @@
 
 describe('GOV.UK redirect', () => {
   it('should redirect / to gov.uk site', () => {
-    cy.visit('/') // change URL to match your dev URL
 
-    cy.location('href').should('equal', 'https://www.gov.uk/help-with-prison-visits')
+    cy.request({
+      url: '/',
+      followRedirect: false,
+    }).then((resp) => {
+      expect(resp.status).to.eq(302)
+      expect(resp.redirectedToUrl).to.eq('https://www.gov.uk/help-with-prison-visits')
+    })
   })
 })
