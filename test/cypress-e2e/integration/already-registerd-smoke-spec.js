d24148dba5f2583e384ab8e0e7fd51e1c3da72db
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2021-10-18 16:33:15 +0100

    Smoke test for non-existent claim

---
+++ test/cypress-e2e/integration/already-registerd-smoke-spec.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+
+describe('Smoke test', () => {
+  const REFERENCE = 'AAAAAAA'
+
+  it('should display already registered page and check database for non-existent reference', () => {
+    // Start already registered
+    cy.visit('/start-already-registered').contains('Return visitors')
+
+    cy.get('[data-cy=reference]').type(REFERENCE)
+    cy.get('[data-cy=dob-day]').type('13')
+    cy.get('[data-cy=dob-month]').type('1')
+    cy.get('[data-cy=dob-year]').type('1980')
+
+    cy.get('[data-cy=submit]').contains('Continue').click()
+
+    cy.title().should('eq', 'Return visitor sign in - Get help with the cost of prison visits')
+    cy.contains('Could not find any claims for these details')
+  })
+})
