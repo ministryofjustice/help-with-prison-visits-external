0a80670e73f437f0d421692955ff778ac71a9174
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2021-12-21 10:42:41 +0000

    Rework clamscan code

--- app/services/clam-av.js
+++ app/services/clam-av.js
@@ -3,40 +3,39 @@ const Promise = require('bluebird').Promise
 const NodeClam = require('clamscan')
 const log = require('./log')
 
-let clam
-try {
-  clam = new NodeClam().init({
-    clamdscan: {
-      host: config.CLAM_AV_HOST,
-      port: config.CLAM_AV_PORT,
-      timeout: config.CLAM_AV_TIMEOUT
-    }
-  })
-} catch (error) {
-  // Suppress clamscan error if disabled
-  console.log(error)
-  if (config.ENABLE_MALWARE_SCANNING !== 'true' &&
-      !error.message.includes('No valid & active virus scanning binaries are active and available and no host/socket option provided!')) {
-    throw error
-  }
-}
-
 module.exports.scan = async function (filePath) {
   if (config.ENABLE_MALWARE_SCANNING === 'true') {
-    clam.then(async clamscan => {
-      try {
-        const {isInfected, file, viruses} = await clamscan.isInfected(filePath) //eslint-disable-line
-        return isInfected //eslint-disable-line
-      } catch (err) {
-        log.error('Error thrown during clamav scan')
+    try {
+      const clam = new NodeClam().init({
+        clamdscan: {
+          host: config.CLAM_AV_HOST,
+          port: config.CLAM_AV_PORT,
+          timeout: config.CLAM_AV_TIMEOUT
+        }
+      })
+      clam.then(async clamscan => {
+        try {
+          const {isInfected, file, viruses} = await clamscan.isInfected(filePath) //eslint-disable-line
+          log.info('clamav: File scanned')
+          return isInfected //eslint-disable-line
+        } catch (err) {
+          log.error('Error thrown during clamav scan')
+          log.error(err)
+          throw err
+        }
+      }).catch(err => {
+        log.error('Error thrown during clamav initialisation')
         log.error(err)
         throw err
+      })
+    } catch (error) {
+      // Suppress clamscan error if disabled
+      log.error(error)
+      if (config.ENABLE_MALWARE_SCANNING !== 'true' &&
+          !error.message.includes('No valid & active virus scanning binaries are active and available and no host/socket option provided!')) {
+        throw error
       }
-    }).catch(err => {
-      log.error('Error thrown during clamav initialisation')
-      log.error(err)
-      throw err
-    })
+    }
   } else {
     return Promise.resolve(false)
   }
