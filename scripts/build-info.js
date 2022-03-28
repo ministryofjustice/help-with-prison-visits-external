142b74bc9716b6f37e96003d1f5858165983a34c
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2022-03-28 16:08:29 +0100

    Update build to match hwpv internal

---
+++ scripts/build-info.js
@@ -0,0 +1,17 @@
+/* eslint-disable consistent-return */
+
+const fs = require('fs')
+const { dirname } = require('path')
+const mkdirp = require('mkdirp')
+
+function writeFile (path, contents, callback) {
+  mkdirp(dirname(path), err => {
+    if (err) return callback(err)
+
+    fs.writeFile(path, contents, callback)
+  })
+}
+
+module.exports.recordBuildInfoTo = (target, contents, callback) => {
+  writeFile(target, JSON.stringify(contents, null, 2), callback)
+}
