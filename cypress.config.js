46b58d54d3475e025489f1dceacd504603535d56
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2022-10-13 07:37:20 +0100

    Cypress 9 to 10 upgrade

---
+++ cypress.config.js
@@ -0,0 +1,19 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  fixturesFolder: 'test/cypress-e2e/fixtures',
+  screenshotsFolder: 'test/cypress-e2e/screenshots',
+  videosFolder: 'test/cypress-e2e/videos',
+  viewportWidth: 1200,
+  viewportHeight: 1400,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents (on, config) {
+      return require('./test/cypress-e2e/plugins/index.js')(on, config)
+    },
+    baseUrl: 'http://localhost:3000',
+    specPattern: 'test/cypress-e2e/integration/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: 'test/cypress-e2e/support/index.js'
+  }
+})
