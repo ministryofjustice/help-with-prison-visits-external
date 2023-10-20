b0120125d33a24494c091f67a8476aac3ddbfcca
Paul Solecki <51918433+psoleckimoj@users.noreply.github.com>
2023-10-20 09:34:24 +0100

    Add health endpoint (#846)

--- app/routes/health-check/status.js
+++ app/routes/health-check/status.js
@@ -2,4 +2,9 @@ module.exports = function (router) {
   router.get('/status', function (req, res) {
     return res.sendStatus(200)
   })
+  router.get('/health', function (req, res) {
+    return res.json({
+      status: 'UP'
+    })
+  })
 }
