c2fe857395a84863a37dd5516a46d7fe460483e5
Simon Nebesnuick <118196897+simonnebMOJ@users.noreply.github.com>
2025-03-27 11:24:01 +0000

    Fixed poorly rendered text for when a contact has been updated (#899)

--- app/constants/claim-event-enum.js
+++ app/constants/claim-event-enum.js
@@ -2,5 +2,6 @@ module.exports = {
   'CLAIM-APPROVED': 'Claim approved',
   'CLAIM-REJECTED': 'Claim rejected',
   'CLAIM-REQUEST-INFORMATION': 'Requested Information',
-  'CLOSE-ADVANCE-CLAIM': 'Advance claim closed'
+  'CLOSE-ADVANCE-CLAIM': 'Advance claim closed',
+  'UPDATED-CONTACT-DETAILS': 'Contact details updated'
 }
--- test/unit/views/helpers/test-claim-event-helper.js
+++ test/unit/views/helpers/test-claim-event-helper.js
@@ -4,6 +4,8 @@ describe('views/helpers/claim-event-helper', function () {
   const APPROVED = 'CLAIM-APPROVED'
   const REQUEST_INFORMATION = 'CLAIM-REQUEST-INFORMATION'
   const REJECTED = 'CLAIM-REJECTED'
+  const REQUESTED = 'CLAIM-REQUEST-INFORMATION'
+  const CONTACT = 'UPDATED-CONTACT-DETAILS'
   const NON_MATCHING = ''
 
   it(`should return the expected value when passed ${APPROVED}`, function () {
@@ -16,6 +18,14 @@ describe('views/helpers/claim-event-helper', function () {
 
   it(`should return the expected value when passed ${REJECTED}`, function () {
     expect(claimEventHelper(REJECTED)).toBe('Claim rejected')
+  })
+
+  it(`should return the expected value when passed ${REQUESTED}`, function () {
+    expect(claimEventHelper(REQUESTED)).toBe('Requested Information')
+  })
+
+  it(`should return the expected value when passed ${CONTACT}`, function () {
+    expect(claimEventHelper(CONTACT)).toBe('Contact details updated')
   })
 
   it('should return the original input if passed a non-matching value', function () {
