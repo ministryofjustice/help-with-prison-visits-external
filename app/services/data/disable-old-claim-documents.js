b1ea9d68e3e2208b20cc6fa1eb625de38e022992
Paul Solecki <51918433+psoleckimoj@users.noreply.github.com>
2025-11-04 17:53:35 +0000

    Revert "Deal with expected missing uploaded files better (#968)"
    This reverts commit 1168c4fc7268de9ad580e7c33890dd31abf8898f.

--- app/services/data/disable-old-claim-documents.js
+++ app/services/data/disable-old-claim-documents.js
@@ -1,23 +1,20 @@
 const { getDatabaseConnector } = require('../../databaseConnector')
 const FileUpload = require('../domain/file-upload')
 
-module.exports = (reference, claimId, fileUpload, multipageDoc, fileUploadSupplied = true) => {
-  if (fileUploadSupplied && !(fileUpload instanceof FileUpload)) {
-    throw new Error('Provided fileUpload object is not an instance of the expected class (Disable old claim)')
+module.exports = (reference, claimId, fileUpload, multipageDoc) => {
+  if (!(fileUpload instanceof FileUpload)) {
+    throw new Error('Provided fileUpload object is not an instance of the expected class')
   }
 
   if (multipageDoc) {
     return Promise.resolve()
   }
-
   const db = getDatabaseConnector()
 
-  return db('ClaimDocument')
-    .update('IsEnabled', false)
-    .where({
-      Reference: reference,
-      ClaimId: claimId,
-      ClaimExpenseId: fileUpload.claimExpenseId ?? 0,
-      DocumentType: fileUpload.documentType ?? '',
-    })
+  return db('ClaimDocument').update('IsEnabled', false).where({
+    Reference: reference,
+    ClaimId: claimId,
+    ClaimExpenseId: fileUpload.claimExpenseId,
+    DocumentType: fileUpload.documentType,
+  })
 }
