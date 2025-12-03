eec0cc96c692c6bc4468fb213d62bc2d483ad9b3
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2025-12-03 17:01:24 +0000

    HWPV-281: Add standard monitoring library

--- app/routes/health-check/status.js
+++ app/routes/health-check/status.js
@@ -1,10 +1,18 @@
+const { monitoringMiddleware, endpointHealthComponent } = require('@ministryofjustice/hmpps-monitoring')
+
+const log = require('../../services/log')
+const config = require('../../../config')
+const applicationInfo = require('../../application-version')
+
 module.exports = router => {
-  router.get('/status', (req, res) => {
-    return res.sendStatus(200)
+  const apiConfig = Object.entries(config.apis)
+
+  const middleware = monitoringMiddleware({
+    applicationInfo,
+    healthComponents: apiConfig.map(([name, options]) => endpointHealthComponent(log, name, options)),
   })
-  router.get('/health', (req, res) => {
-    return res.json({
-      status: 'UP',
-    })
-  })
+
+  router.get('/health', middleware.health)
+  router.get('/info', middleware.info)
+  router.get('/ping', middleware.ping)
 }
