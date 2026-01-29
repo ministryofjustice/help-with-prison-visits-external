8c66583cf041312565319afb05b8e94c3fc9baa6
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2026-01-29 12:52:43 +0000

    Fix dependencies

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -2,7 +2,7 @@ import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-all
 
 export default configureAllowedScripts({
   allowlist: {
-    'node_modules/@parcel/watcher@2.5.4': 'ALLOW',
+    'node_modules/@parcel/watcher@2.5.6': 'ALLOW',
     'node_modules/cypress@15.9.0': 'ALLOW',
     'node_modules/dtrace-provider@0.8.8': 'ALLOW',
     'node_modules/fsevents@2.3.3': 'ALLOW',
