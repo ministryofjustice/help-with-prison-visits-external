d4f6ac8d326e3b9cfece99fc888597d2bd7d67ea
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2026-04-21 16:42:08 +0100

    Forbid modules

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -4,8 +4,8 @@ export default configureAllowedScripts({
   allowlist: {
     'node_modules/@parcel/watcher@2.5.6': 'ALLOW',
     'node_modules/cypress@15.13.1': 'ALLOW',
-    'node_modules/dtrace-provider@0.8.8': 'ALLOW',
-    'node_modules/fsevents@2.3.3': 'ALLOW',
+    'node_modules/dtrace-provider@^0.8.8': 'FORBID',
+    'node_modules/fsevents@^2.3.3': 'FORBID',
     'node_modules/unrs-resolver@1.11.1': 'ALLOW',
   },
 })
