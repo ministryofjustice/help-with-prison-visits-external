d5ef4531d9e4ba0146a341beb3844f126f381ac1
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2026-06-17 08:08:22 +0100

    Fix security vulnerabilities 2026-06-17

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -6,6 +6,6 @@ export default configureAllowedScripts({
     'node_modules/cypress@^15.15.0': 'ALLOW',
     'node_modules/dtrace-provider@0.8.8': 'FORBID',
     'node_modules/fsevents@2.3.3': 'FORBID',
-    'node_modules/unrs-resolver@1.11.1': 'ALLOW',
+    'node_modules/unrs-resolver@1.12.2': 'ALLOW',
   },
 })
